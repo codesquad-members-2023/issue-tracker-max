a204c871bbbe156b8f749f0c4bddfd05d4eb7f91
bakhacode <bakhamitai@naver.com>
2023-07-24 21:00:51 +0900

    chore: 개발 환경 세팅
    - vite기반 개발 환경 구축
    - emotion 및 라이브러리 의존성 추가

---
+++ fe/src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { LoginPage } from "./pages/LoginPage";
+import { MainPage } from "./pages/MainPage";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ fe/src/pages/MainPage.tsx
@@ -0,0 +1,7 @@
+export function MainPage() {
+  return (
+    <>
+      <h1>Main Page</h1>
+    </>
+  );
+}
