2d33f44cf2077463d287dd188ab3bcfe69fb60e7
jsh3418 <albert5428@gmail.com>
2023-07-25 17:55:16 +0900

    [FE] chore: react router 패키지 추가, BrowserRouter 작성

--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { App } from './App.tsx';
 import './index.css';
 import { worker } from './mocks/worker.ts';
+import { BrowserRouter } from 'react-router-dom';
 
 if (process.env.NODE_ENV === 'development') {
   worker.start();
@@ -10,6 +11,8 @@ if (process.env.NODE_ENV === 'development') {
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
