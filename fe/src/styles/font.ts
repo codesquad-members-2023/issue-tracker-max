802b1cfb3dda4a974d98b01851041fc3ed82520c
TOKO <101464713+aaaz425@users.noreply.github.com>
2023-07-27 17:37:47 +0900

    feat #1: 프로젝트 초기 세팅 (#2)

---
+++ fe/src/styles/font.ts
@@ -0,0 +1,28 @@
+const FONT_BASE = {
+  bold32: '700 32px/48px Pretendard, sans-serif',
+  bold24: '700 24px/36px Pretendard, sans-serif',
+  bold20: '700 20px/32px Pretendard, sans-serif',
+  bold16: '700 16px/24px Pretendard, sans-serif',
+  bold12: '700 12px/16px Pretendard, sans-serif',
+  medium32: '500 32px/48px Pretendard, sans-serif',
+  medium24: '500 24px/36px Pretendard, sans-serif',
+  medium20: '500 20px/32px Pretendard, sans-serif',
+  medium16: '500 16px/24px Pretendard, sans-serif',
+  medium12: '500 12px/16px Pretendard, sans-serif',
+};
+
+export const font = {
+  displayBold32: FONT_BASE.bold32,
+  displayBold20: FONT_BASE.bold20,
+  displayBold16: FONT_BASE.bold16,
+  displayBold12: FONT_BASE.bold12,
+  displayMedium20: FONT_BASE.medium20,
+  displayMedium16: FONT_BASE.medium16,
+  displayMedium12: FONT_BASE.medium12,
+  selectedBold20: FONT_BASE.bold20,
+  selectedBold16: FONT_BASE.bold16,
+  selectedBold12: FONT_BASE.bold12,
+  availableMedium20: FONT_BASE.medium20,
+  availableMedium16: FONT_BASE.medium16,
+  availableMedium12: FONT_BASE.medium12,
+};
---
+++ fe/src/styles/object.ts
@@ -0,0 +1,23 @@
+export const radius = {
+  half: '50%',
+  medium: '12px',
+  large: '16px',
+};
+
+export const border = {
+  default: '1px solid #D9DBE9',
+  icon: '1.6px solid #D9DBE9',
+  dash: '1px dash #D9DBE9',
+};
+
+export const opacity = {
+  default: '1',
+  hover: '0.8',
+  press: '0.64',
+  disabled: '0.32',
+};
+
+export const shadow = {
+  light: '0px 0px 8px 0px #14142B0A',
+  dark: '0px 0px 16px 0px #14142BCC',
+};
