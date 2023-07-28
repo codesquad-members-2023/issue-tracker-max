f8e89bc8a993a48f5003e5049db0b59c0624760c
JIHYE <jdh324@naver.com>
2023-07-28 11:11:52 +0900

    ✨feat #17: 이슈 등록 service 구현

--- backend/src/main/java/codesquard/app/issue/service/IssueService.java
+++ backend/src/main/java/codesquard/app/issue/service/IssueService.java
@@ -1,7 +1,10 @@
 package codesquard.app.issue.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import codesquard.app.issue.dto.request.IssueRegisterRequest;
+import codesquard.app.issue.entity.Issue;
 import codesquard.app.issue.repository.IssueRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -10,4 +13,22 @@ import lombok.RequiredArgsConstructor;
 public class IssueService {
 
 	private final IssueRepository issueRepository;
+
+	@Transactional
+	public Long register(IssueRegisterRequest issueRegisterRequest, Long userId) {
+		Issue issue = Issue.builder()
+			.userId(userId)
+			.title(issueRegisterRequest.getTitle())
+			.content(issueRegisterRequest.getContent())
+			.milestoneId(issueRegisterRequest.getMilestone())
+			.build();
+		Long id = issueRepository.save(issue);
+		for (Long labelId : issueRegisterRequest.getLabels()) {
+			issueRepository.saveIssueLabel(id, labelId);
+		}
+		for (Long assignee : issueRegisterRequest.getAssignees()) {
+			issueRepository.saveIssueAssignee(id, assignee);
+		}
+		return id;
+	}
 }
