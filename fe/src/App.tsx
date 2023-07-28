7eee182cdef63ca225c521ea191cc4801c7ab3f8
jsh3418 <albert5428@gmail.com>
2023-07-28 12:27:01 +0900

    [FE] feat: react router 구현

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,6 +1,14 @@
+import { Header } from '@components/header/Header';
 import { ThemeProvider } from '@emotion/react';
+import { AddIssuePage } from '@pages/AddIssuePage';
+import { IssueDetailPage } from '@pages/IssueDetailPage';
+import { IssueListPage } from '@pages/IssueListPage';
+import { LabelListPage } from '@pages/LabelListPage';
+import { MileStoneListPage } from '@pages/MileStoneListPage';
+import { SignPage } from '@pages/SignPage';
 import { darkMode, lightMode } from '@styles/designSystem';
 import { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 export const App: React.FC = () => {
   const [userId, setUserId] = useState();
@@ -14,7 +22,18 @@ export const App: React.FC = () => {
 
   return (
     <ThemeProvider theme={theme}>
-      <div>Hello world</div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/sign" element={<SignPage />} />
+          <Route path="/" element={<Header />}>
+            <Route index element={<IssueListPage />} />
+            <Route path="add" element={<AddIssuePage />} />
+            <Route path="issue/:id" element={<IssueDetailPage />} />
+            <Route path="label" element={<LabelListPage />} />
+            <Route path="milestone" element={<MileStoneListPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 };
