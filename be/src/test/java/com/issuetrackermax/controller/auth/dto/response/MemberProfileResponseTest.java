2abfefb2c309c4566f55efe4d4d54fbd2f673320
JJONSOO <pjs970616@naver.com>
2023-07-28 22:42:44 +0900

    feat #8 : Domain Test Code 작성

---
+++ be/src/test/java/com/issuetrackermax/controller/auth/dto/response/MemberProfileResponseTest.java
@@ -0,0 +1,26 @@
+package com.issuetrackermax.controller.auth.dto.response;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.issuetrackermax.domain.member.Entity.LoginType;
+import com.issuetrackermax.domain.member.Entity.Member;
+
+class MemberProfileResponseTest {
+
+	@DisplayName("MemberProfileResponse에서 loginType이 Github인 Member를 만들어준다.")
+	@Test
+	void memberProfileResponseToMember() {
+		// given
+		MemberProfileResponse memberProfileResponse = MemberProfileResponse.builder().loginId("June").build();
+		// when
+		Member member = memberProfileResponse.toMember();
+		// then
+		assertThat(member.getLoginType()).isEqualTo(LoginType.GITHUB);
+		assertThat(member.getLoginId()).isEqualTo("June");
+
+	}
+
+}
