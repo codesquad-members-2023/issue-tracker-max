102cea2fb759a7d28be1de015dcf8142dac461cc
hjsong <song6240@naver.com>
2023-07-29 13:34:56 +0900

    ✨feat #18: TabButton 컴포넌트 구현
    - GhostButton 활용
    - selected가 truthy일 경우 Button에 className 'selected' 추가

---
+++ frontend/src/components/TabButton.tsx
@@ -0,0 +1,65 @@
+import { styled } from "styled-components";
+import { Button } from "./Button";
+
+export function TabButton({
+  tabs,
+  onClick,
+}: {
+  tabs: {
+    name: string;
+    icon?: string;
+    selected?: boolean;
+  }[];
+  onClick: (name: string) => void;
+}) {
+  const buttons = tabs.map(({ name, icon, selected }) => (
+    <Button
+      key={name}
+      icon={icon}
+      size="M"
+      buttonType="Ghost"
+      flexible="Flexible"
+      selected={selected}
+      onClick={() => onClick(name)}
+    >
+      {name}
+    </Button>
+  ));
+
+  return <StyledTabButton>{buttons}</StyledTabButton>;
+}
+
+const StyledTabButton = styled.div`
+  display: flex;
+  align-items: center;
+  width: 320px;
+  height: 40px;
+  border: ${({ theme }) =>
+    `${theme.border.default} ${theme.color.neutralBorderDefault}`};
+  border-radius: ${({ theme }) => theme.radius.medium};
+  background-color: ${({ theme }) => theme.color.neutralSurfaceDefault};
+
+  & button {
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+
+    &:first-child {
+      border-right: ${({ theme }) =>
+        `${theme.border.default} ${theme.color.neutralBorderDefault}`};
+      border-radius: ${({ theme }) =>
+        `${theme.radius.medium} 0 0 ${theme.radius.medium}`};
+    }
+
+    &:last-child {
+      border-radius: ${({ theme }) =>
+        `0 ${theme.radius.medium} ${theme.radius.medium} 0`};
+    }
+
+    &.selected {
+      background-color: ${({ theme }) => theme.color.neutralSurfaceBold};
+    }
+  }
+`;
