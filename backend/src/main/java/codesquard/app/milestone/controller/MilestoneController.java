9b5572aff6815ee52312c5cf2db0cc8b88be3387
Sully <2dcoder@naver.com>
2023-08-01 16:09:19 +0900

    :sparkles:feat #37: 마일스톤 수정 기능 Controller 단계 구현

--- backend/src/main/java/codesquard/app/milestone/controller/MilestoneController.java
+++ backend/src/main/java/codesquard/app/milestone/controller/MilestoneController.java
@@ -4,12 +4,16 @@ import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import codesquard.app.milestone.dto.request.MilestoneSaveRequest;
+import codesquard.app.milestone.dto.request.MilestoneUpdateRequest;
 import codesquard.app.milestone.dto.response.MilestoneSaveResponse;
+import codesquard.app.milestone.dto.response.MilestoneUpdateResponse;
 import codesquard.app.milestone.service.MilestoneService;
 
 @RestController
@@ -25,5 +29,14 @@ public class MilestoneController {
 		Long milestoneId = milestoneService.saveMilestone(milestoneSaveRequest);
 		return ResponseEntity.status(HttpStatus.CREATED)
 			.body(MilestoneSaveResponse.success(true, milestoneId));
+	}
+
+	@PutMapping("/api/milestones/{milestoneId}")
+	public ResponseEntity<MilestoneUpdateResponse> update(@PathVariable final Long milestoneId,
+		@Valid @RequestBody MilestoneUpdateRequest milestoneUpdateRequest) {
+		milestoneService.updateMilestone(milestoneId, milestoneUpdateRequest);
+
+		return ResponseEntity.status(HttpStatus.OK)
+			.body(MilestoneUpdateResponse.success(true));
 	}
 }
