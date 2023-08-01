751533efc7d41466ca8f08116364cc096efc3983
sejeong00 <buttermelt@daum.net>
2023-08-02 01:41:39 +0900

    [BE] feat: issue 생성 기능 구현 (#30)

--- be/issue-tracker/src/main/java/org/presents/issuetracker/issue/controller/IssueController.java
+++ be/issue-tracker/src/main/java/org/presents/issuetracker/issue/controller/IssueController.java
@@ -1,7 +1,26 @@
 package org.presents.issuetracker.issue.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.presents.issuetracker.global.dto.response.IdResponseDto;
+import org.presents.issuetracker.issue.dto.request.IssueCreateRequestDto;
+import org.presents.issuetracker.issue.service.IssueService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/issues")
+@RequiredArgsConstructor
 public class IssueController {
+    private final IssueService issueService;
+
+    @PostMapping("/new")
+    public ResponseEntity<IdResponseDto> create(@RequestBody IssueCreateRequestDto issueCreateRequestDto) {
+        IdResponseDto issueCreateResponse = IdResponseDto.builder().id(issueService.create(issueCreateRequestDto)).build();
+
+        return ResponseEntity.status(HttpStatus.OK).body(issueCreateResponse);
+    }
 }
--- be/issue-tracker/src/main/java/org/presents/issuetracker/issue/service/IssueService.java
+++ be/issue-tracker/src/main/java/org/presents/issuetracker/issue/service/IssueService.java
@@ -1,7 +1,66 @@
 package org.presents.issuetracker.issue.service;
 
+import lombok.RequiredArgsConstructor;
+import org.presents.issuetracker.issue.dto.request.IssueCreateRequestDto;
+import org.presents.issuetracker.issue.entity.Assignee;
+import org.presents.issuetracker.issue.entity.Issue;
+import org.presents.issuetracker.issue.entity.IssueLabel;
+import org.presents.issuetracker.issue.repository.IssueRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.stream.Collectors;
+
 @Service
+@RequiredArgsConstructor
 public class IssueService {
+
+    private final IssueRepository issueRepository;
+
+    public Long create(IssueCreateRequestDto issueCreateRequestDto) {
+        Long savedIssueId = issueRepository.save(
+                Issue.builder()
+                        .title(issueCreateRequestDto.getTitle())
+                        .authorId(issueCreateRequestDto.getAuthorId())
+                        .contents(issueCreateRequestDto.getContents())
+                        .build()
+        );
+
+        //TODO: 만약 assignee가 지정이 안된다면 assignee:null인지 아니면 아예 assignee라는 key가 없는지
+        if(issueCreateRequestDto.getAssigneeIds() != null) {
+            issueRepository.deleteAllAssignee(savedIssueId);
+            issueRepository.addAssignee(
+                    issueCreateRequestDto.getAssigneeIds().stream()
+                            .map(assigneeId ->
+                                    Assignee.builder()
+                                            .issueId(savedIssueId)
+                                            .userId(assigneeId)
+                                            .build()
+                            )
+                            .collect(Collectors.toList())
+            );
+        }
+
+        if(issueCreateRequestDto.getLabelIds() != null) {
+            issueRepository.deleteAllLabel(savedIssueId);
+            issueRepository.addLabel(
+              issueCreateRequestDto.getLabelIds().stream()
+                      .map(labelId ->
+                                      IssueLabel.builder()
+                                              .issueId(savedIssueId)
+                                              .labelId(labelId)
+                                              .build()
+                              )
+                      .collect(Collectors.toList())
+            );
+        }
+
+        if(issueCreateRequestDto.getMilestoneId() != null) {
+            issueRepository.setMilestone(
+                    savedIssueId, issueCreateRequestDto.getMilestoneId()
+            );
+        }
+
+        return savedIssueId;
+    }
+
 }
