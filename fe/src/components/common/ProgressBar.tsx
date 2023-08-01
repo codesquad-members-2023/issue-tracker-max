d24f6b9bb0dabf34f5caadab062f6a84d3bc3b44
JH <vvaa277@gmail.com>
2023-08-01 18:30:24 +0900

    [FE] feat: ProgressBar 컴포넌트 구현 (#24)

---
+++ fe/src/components/common/ProgressBar.tsx
@@ -0,0 +1,22 @@
+import { useTheme } from '@emotion/react';
+
+type Props = {
+  progress?: number;
+};
+
+export const ProgressBar: React.FC<Props> = ({ progress }) => {
+  const theme = useTheme() as any;
+
+  return (
+    <>
+      <div
+        css={{
+          width: '224px',
+          height: '8px',
+          borderRadius: '10px',
+          background: `linear-gradient(90deg, ${theme.palette.blue} 0%, ${theme.palette.blue} ${progress}%, ${theme.neutral.surface.bold} ${progress}%, ${theme.neutral.surface.bold} 100%)`,
+        }}
+      ></div>
+    </>
+  );
+};
---
+++ fe/src/components/common/ProgressLabel.tsx
@@ -0,0 +1,57 @@
+import { useTheme } from '@emotion/react';
+
+type Props = {
+  name?: string;
+  progress?: number;
+  openIssueCount?: number;
+  closeIssueCount?: number;
+};
+
+export const ProgressLabel: React.FC<Props> = ({
+  name,
+  progress,
+  openIssueCount,
+  closeIssueCount,
+}) => {
+  const theme = useTheme() as any;
+
+  return (
+    <>
+      {name && (
+        <p
+          css={{
+            marginTop: '8px',
+            font: theme.fonts.displayMedium12,
+            color: theme.neutral.text.strong,
+          }}
+        >
+          {name}
+        </p>
+      )}
+
+      {progress && openIssueCount && closeIssueCount && (
+        <div
+          css={{
+            marginTop: '8px',
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'space-between',
+            font: theme.fonts.displayMedium12,
+            color: theme.neutral.text.weak,
+          }}
+        >
+          <p>{progress}%</p>
+          <div
+            css={{
+              display: 'flex',
+              gap: '8px',
+            }}
+          >
+            <p>열린 이슈 {openIssueCount}</p>
+            <p>닫힌 이슈 {closeIssueCount}</p>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
