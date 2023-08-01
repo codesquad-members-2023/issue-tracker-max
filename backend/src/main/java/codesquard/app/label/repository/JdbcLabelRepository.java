1e8599e13765bb8a4c6d078760ec569e5b237134
Sully <2dcoder@naver.com>
2023-08-01 15:08:21 +0900

    ♻️refactor #40: param 코드 리팩토링

--- backend/src/main/java/codesquard/app/label/repository/JdbcLabelRepository.java
+++ backend/src/main/java/codesquard/app/label/repository/JdbcLabelRepository.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
@@ -25,13 +26,12 @@ public class JdbcLabelRepository implements LabelRepository {
 		String sql = "INSERT INTO `label` (`name`, `color`, `background`, `description`) "
 			+ "VALUES (:name, :color, :background, :description)";
 
-		// TODO: enum 타입은 자동으로 못 가져오니 하나씩 내가 직접 넣어줘야 할 듯
-		// SqlParameterSource param = new BeanPropertySqlParameterSource(label);
-		MapSqlParameterSource param = new MapSqlParameterSource();
-		param.addValue("name", label.getName());
-		param.addValue("color", label.getColor().getNameToLowerCase());
-		param.addValue("background", label.getBackground());
-		param.addValue("description", label.getDescription());
+		SqlParameterSource param = new MapSqlParameterSource()
+			.addValue("name", label.getName())
+			.addValue("color", label.getColor().getNameToLowerCase())
+			.addValue("background", label.getBackground())
+			.addValue("description", label.getDescription());
+
 		KeyHolder keyHolder = new GeneratedKeyHolder();
 
 		template.update(sql, param, keyHolder);
