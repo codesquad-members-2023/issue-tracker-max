3a176101da341ad1923ebd58bf2ff99257eb58ed
Sully <2dcoder@naver.com>
2023-08-01 10:29:35 +0900

    ✨feat #40: 라벨 등록 기능 Service 단계 구현

--- backend/src/main/java/codesquard/app/label/service/LabelService.java
+++ backend/src/main/java/codesquard/app/label/service/LabelService.java
@@ -1,13 +1,23 @@
 package codesquard.app.label.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import codesquard.app.label.dto.LabelRequestDto;
+import codesquard.app.label.entity.Label;
 import codesquard.app.label.repository.LabelRepository;
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 @Service
+@Transactional
 public class LabelService {
-	
 	private final LabelRepository labelRepository;
+
+	public LabelService(LabelRepository labelRepository) {
+		this.labelRepository = labelRepository;
+	}
+
+	public Long saveLabel(LabelRequestDto labelRequestDto) {
+		Label label = LabelRequestDto.toEntity(labelRequestDto);
+		return labelRepository.save(label).orElseThrow(() -> new RuntimeException("임시"));
+	}
 }
