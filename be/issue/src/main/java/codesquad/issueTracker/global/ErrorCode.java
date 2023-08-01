2318200a0bb61e91266294f687e75ba7dad0eb3b
DOEKYONG <apochmn@naver.com>
2023-08-01 11:44:06 +0900

    #15 fix : ErrorCode 추가 및 응답코드 수정

--- be/issue/src/main/java/codesquad/issueTracker/global/ErrorCode.java
+++ be/issue/src/main/java/codesquad/issueTracker/global/ErrorCode.java
@@ -4,30 +4,32 @@ import org.springframework.http.HttpStatus;
 
 public enum ErrorCode implements StatusCode {
 
-    REQUEST_VALIDATION_FAIL(HttpStatus.BAD_REQUEST),
+	REQUEST_VALIDATION_FAIL(HttpStatus.BAD_REQUEST),
 
-    // -- [USER] -- //
-    ALREADY_EXIST_USER(HttpStatus.BAD_REQUEST, "이미 존재하는 유저입니다.");
+	// -- [USER] -- //
+	ALREADY_EXIST_USER(HttpStatus.BAD_REQUEST, "이미 존재하는 유저입니다."),
+	NOT_FOUND_USER(HttpStatus.BAD_REQUEST, "해당하는 유저가 없습니다."),
+	FAILED_LOGIN_USER(HttpStatus.BAD_REQUEST, "로그인에 실패했습니다. 아이디, 비밀번호를 다시 입력해주세요. ");
 
-    private HttpStatus status;
-    private String message;
+	private HttpStatus status;
+	private String message;
 
-    ErrorCode(HttpStatus status, String message) {
-        this.status = status;
-        this.message = message;
-    }
+	ErrorCode(HttpStatus status, String message) {
+		this.status = status;
+		this.message = message;
+	}
 
-    ErrorCode(HttpStatus status) {
-        this.status = status;
-    }
+	ErrorCode(HttpStatus status) {
+		this.status = status;
+	}
 
-    @Override
-    public HttpStatus getStatus() {
-        return this.status;
-    }
+	@Override
+	public HttpStatus getStatus() {
+		return this.status;
+	}
 
-    @Override
-    public String getMessage() {
-        return this.message;
-    }
+	@Override
+	public String getMessage() {
+		return this.message;
+	}
 }
--- be/issue/src/main/java/codesquad/issueTracker/global/GlobalExceptionHandler.java
+++ be/issue/src/main/java/codesquad/issueTracker/global/GlobalExceptionHandler.java
@@ -1,36 +1,40 @@
 package codesquad.issueTracker.global;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.List;
-
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(CustomException.class)
-    public ApiResponse<String> handleCustomException(CustomException e) {
-        StatusCode statusCode = e.getStatusCode();
+	@ExceptionHandler(CustomException.class)
+	public ResponseEntity<ApiResponse<String>> handleCustomException(CustomException e) {
+		StatusCode statusCode = e.getStatusCode();
 
-        return ApiResponse.fail(statusCode.getStatus(), statusCode.getMessage());
-    }
+		return ResponseEntity.status(statusCode.getStatus())
+			.body(ApiResponse.fail(statusCode.getStatus(), statusCode.getMessage()));
+	}
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ApiResponse<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        StatusCode statusCode = ErrorCode.REQUEST_VALIDATION_FAIL;
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ApiResponse<String>> handleMethodArgumentNotValidException(
+		MethodArgumentNotValidException e) {
+		StatusCode statusCode = ErrorCode.REQUEST_VALIDATION_FAIL;
 
-        List<ObjectError> objectErrors = e.getBindingResult().getAllErrors();
+		List<ObjectError> objectErrors = e.getBindingResult().getAllErrors();
 
-        StringBuilder errorMessage = new StringBuilder();
-        for (ObjectError error : objectErrors) {
-            errorMessage.append(error.getDefaultMessage()).append(",");
-        }
-        errorMessage.deleteCharAt(errorMessage.length() - 1);
+		StringBuilder errorMessage = new StringBuilder();
+		for (ObjectError error : objectErrors) {
+			errorMessage.append(error.getDefaultMessage()).append(",");
+		}
+		errorMessage.deleteCharAt(errorMessage.length() - 1);
 
-        return ApiResponse.fail(statusCode.getStatus(), errorMessage.toString());
-    }
+		return ResponseEntity.status(statusCode.getStatus())
+			.body(ApiResponse.fail(statusCode.getStatus(), errorMessage.toString()));
+	}
 
 }
 
