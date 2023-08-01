8f1da586ca72a351e76ec604697baa1b39183ce8
Sully <2dcoder@naver.com>
2023-08-01 10:30:04 +0900

    ✨feat #40: 라벨 등록 기능 Controller 단계 구현

--- backend/src/main/java/codesquard/app/label/controller/LabelController.java
+++ backend/src/main/java/codesquard/app/label/controller/LabelController.java
@@ -1,13 +1,27 @@
 package codesquard.app.label.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import codesquard.app.label.dto.LabelRequestDto;
+import codesquard.app.label.dto.LabelResponseDto;
 import codesquard.app.label.service.LabelService;
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 @RestController
 public class LabelController {
-
 	private final LabelService labelService;
+
+	public LabelController(LabelService labelService) {
+		this.labelService = labelService;
+	}
+
+	@PostMapping("/api/labels")
+	public ResponseEntity<LabelResponseDto> save(@RequestBody LabelRequestDto labelRequestDto) {
+		Long labelId = labelService.saveLabel(labelRequestDto);
+		return ResponseEntity.status(HttpStatus.CREATED)
+			.body(LabelResponseDto.success(true, labelId));
+	}
 }
