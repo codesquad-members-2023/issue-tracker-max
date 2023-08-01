ea9b8ab82ba20fc04f828b2a57647144f0ac1164
JJONSOO <pjs970616@naver.com>
2023-08-01 17:04:00 +0900

    feat #17 : Controller 하나의 SpringBootTest로 통합

--- be/src/test/java/com/issuetrackermax/controller/member/MemberControllerTest.java
+++ be/src/test/java/com/issuetrackermax/controller/member/MemberControllerTest.java
@@ -6,26 +6,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.issuetrackermax.controller.ControllerTestSupport;
 import com.issuetrackermax.controller.member.dto.request.SignUpRequest;
-import com.issuetrackermax.service.member.MemberService;
 
-@WebMvcTest(controllers = MemberController.class)
-class MemberControllerTest {
-	@Autowired
-	private MockMvc mockMvc;
-
-	@Autowired
-	private ObjectMapper objectMapper;
-
-	@MockBean
-	private MemberService memberService;
+class MemberControllerTest extends ControllerTestSupport {
 
 	@DisplayName("이메일, 페스워드, 닉네임을 입력하여 회원가입을 성공한다.")
 	@Test
