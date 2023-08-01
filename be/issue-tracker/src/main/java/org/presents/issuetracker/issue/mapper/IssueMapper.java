88d55c8802a773eda9ffbc56282e7e21ada3443c
sudago <sudago22@gmail.com>
2023-08-01 17:10:52 +0900

    [BE] issue mapper 수정 (#16)

--- be/issue-tracker/src/main/java/org/presents/issuetracker/issue/mapper/IssueMapper.java
+++ be/issue-tracker/src/main/java/org/presents/issuetracker/issue/mapper/IssueMapper.java
@@ -3,9 +3,18 @@ package org.presents.issuetracker.issue.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.presents.issuetracker.issue.dto.IssueDto;
+import org.presents.issuetracker.issue.entity.Issue;
+import org.presents.issuetracker.label.entity.Label;
+import org.presents.issuetracker.milestone.entity.Milestone;
+import org.presents.issuetracker.user.entity.User;
 
 @Mapper
 public interface IssueMapper {
-	List<IssueDto> getIssueList();
+	List<Issue> getIssueList();
+
+	User getUser(Long userId);
+
+	List<Label> getLabelsByIssueId(Long issueId);
+
+	Milestone getMilestoneByIssueId(Long issueId);
 }
