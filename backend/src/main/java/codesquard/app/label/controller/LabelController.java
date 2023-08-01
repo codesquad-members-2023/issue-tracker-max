dc429f5c67d4a5f0952217cadeb211580f69472e
Sully <2dcoder@naver.com>
2023-08-01 16:45:33 +0900

    :sparkles:feat #44: 라벨 수정 기능 Controller 단계 구현

--- backend/src/main/java/codesquard/app/label/controller/LabelController.java
+++ backend/src/main/java/codesquard/app/label/controller/LabelController.java
@@ -4,13 +4,18 @@ import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import codesquard.app.label.dto.LabelSaveRequest;
 import codesquard.app.label.dto.LabelSaveResponse;
+import codesquard.app.label.dto.LabelUpdateRequest;
+import codesquard.app.label.dto.LabelUpdateResponse;
 import codesquard.app.label.service.LabelService;
+import codesquard.app.milestone.dto.request.MilestoneUpdateRequest;
 
 @RestController
 public class LabelController {
@@ -25,5 +30,14 @@ public class LabelController {
 		Long labelId = labelService.saveLabel(labelSaveRequest);
 		return ResponseEntity.status(HttpStatus.CREATED)
 			.body(LabelSaveResponse.success(true, labelId));
+	}
+
+	@PutMapping("/api/labels/{labelId}")
+	public ResponseEntity<LabelUpdateResponse> update(@PathVariable final Long labelId,
+		@Valid @RequestBody LabelUpdateRequest labelUpdateRequest) {
+		labelService.updateLabel(labelId, labelUpdateRequest);
+
+		return ResponseEntity.status(HttpStatus.OK)
+			.body(LabelUpdateResponse.success(true));
 	}
 }
