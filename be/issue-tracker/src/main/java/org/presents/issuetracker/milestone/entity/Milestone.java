04102d1dc9edf2b203daaedd5131fe7ab30ed47d
sudago <sudago22@gmail.com>
2023-08-01 17:08:43 +0900

    [BE] issue와 연관된 entity 생성 (#16)

--- be/issue-tracker/src/main/java/org/presents/issuetracker/milestone/entity/Milestone.java
+++ be/issue-tracker/src/main/java/org/presents/issuetracker/milestone/entity/Milestone.java
@@ -1,4 +1,16 @@
 package org.presents.issuetracker.milestone.entity;
 
+import java.time.LocalDateTime;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Milestone {
+	private Long milestoneId;
+	private String name;
+	private LocalDateTime deadline;
+	private String description;
+	private String status;
 }
