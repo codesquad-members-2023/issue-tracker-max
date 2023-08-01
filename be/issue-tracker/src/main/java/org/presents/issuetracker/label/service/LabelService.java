8ac80d47ea087e4c1b04ef7f9958b1c1bc4a72dd
dokkisan <dokkisan.wang@gmail.com>
2023-08-01 15:06:17 +0900

    feat: Label 생성 API 구현 (#11)

--- be/issue-tracker/src/main/java/org/presents/issuetracker/label/service/LabelService.java
+++ be/issue-tracker/src/main/java/org/presents/issuetracker/label/service/LabelService.java
@@ -1,7 +1,25 @@
 package org.presents.issuetracker.label.service;
 
+import org.presents.issuetracker.label.dto.request.LabelRequestDto;
+import org.presents.issuetracker.label.entity.Label;
+import org.presents.issuetracker.label.repository.LabelRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LabelService {
+
+    private final LabelRepository labelRepository;
+
+    public LabelService(LabelRepository labelRepository) {
+        this.labelRepository = labelRepository;
+    }
+
+    public Long create(LabelRequestDto labelRequestDto) {
+        return labelRepository.save(
+                Label.of(
+                        labelRequestDto.getName(),
+                        labelRequestDto.getDescription(),
+                        labelRequestDto.getBackgroundColor(),
+                        labelRequestDto.getTextColor()));
+    }
 }
