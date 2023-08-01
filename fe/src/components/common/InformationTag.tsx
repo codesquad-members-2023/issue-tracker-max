e937c522891e21d354cf17ce0425a85d9b80ee72
JH <vvaa277@gmail.com>
2023-08-01 16:14:06 +0900

    [FE] feat: InformationTag 컴포넌트 구현 (#19)

---
+++ fe/src/components/common/InformationTag.tsx
@@ -0,0 +1,65 @@
+import { useTheme } from '@emotion/react';
+
+type Props = {
+  size: 'L' | 'S';
+  typeVariant: 'default' | 'filled';
+  fillColor?: string;
+  textColor?: 'light' | 'dark';
+  children: React.ReactNode;
+};
+
+export const InformationTag: React.FC<Props> = ({
+  size = 'L',
+  typeVariant = 'default',
+  fillColor,
+  textColor,
+  children,
+}) => {
+  const theme = useTheme() as any;
+
+  const TYPE_VARIANT = {
+    default: {
+      backgroundColor: theme.neutral.surface.bold,
+      border: `${theme.border.default} ${theme.neutral.border.default}`,
+    },
+    filled: {
+      backgroundColor: fillColor || theme.neutral.surface.bold,
+      border: `${theme.border.default} ${fillColor}`,
+    },
+  };
+
+  const SIZE = {
+    L: {
+      height: '32px',
+      padding: '0px 16px',
+    },
+    S: {
+      height: '24px',
+      padding: '0px 8px',
+    },
+  };
+
+  return (
+    <div
+      css={{
+        boxSizing: 'border-box',
+        display: 'flex',
+        gap: '4px',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 'fit-content',
+        borderRadius: theme.radius.l,
+        font: theme.fonts.displayMedium12,
+        color:
+          textColor === 'light'
+            ? theme.brand.text.default
+            : theme.neutral.text.weak,
+
+        ...SIZE[size],
+        ...TYPE_VARIANT[typeVariant],
+      }}
+    >
+      {children}
+    </div>
+  );
+};
