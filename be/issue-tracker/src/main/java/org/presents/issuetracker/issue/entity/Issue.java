1e14d0f91465c9fa415d8e1fc49cd5ca24b8d97f
sudago <sudago22@gmail.com>
2023-08-01 17:08:43 +0900

    [BE] issue와 연관된 entity 생성 (#16)

--- be/issue-tracker/src/main/java/org/presents/issuetracker/issue/entity/Issue.java
+++ be/issue-tracker/src/main/java/org/presents/issuetracker/issue/entity/Issue.java
@@ -1,4 +1,18 @@
 package org.presents.issuetracker.issue.entity;
 
+import java.time.LocalDateTime;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Issue {
+	private Long issueId;
+	private Long authorId;
+	private Long milestoneId;
+	private String title;
+	private String contents;
+	private LocalDateTime createdAt;
+	private String status;
 }
--- be/issue-tracker/src/main/java/org/presents/issuetracker/label/entity/Label.java
+++ be/issue-tracker/src/main/java/org/presents/issuetracker/label/entity/Label.java
@@ -1,4 +1,15 @@
 package org.presents.issuetracker.label.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Label {
+	private Long labelId;
+	private String name;
+	private String description;
+	private String backgroundColor;
+	private String textColor;
+	private Boolean isDeleted;
 }
