fa1fbd198c244d1cad6b81faefdd577c062e5b03
Sully <2dcoder@naver.com>
2023-08-02 18:39:32 +0900

    :sparkles:feat #43: 라벨 READ 기능 Repository 단계 구현

--- backend/src/main/java/codesquard/app/label/repository/JdbcLabelRepository.java
+++ backend/src/main/java/codesquard/app/label/repository/JdbcLabelRepository.java
@@ -1,9 +1,11 @@
 package codesquard.app.label.repository;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
@@ -42,12 +44,22 @@ public class JdbcLabelRepository implements LabelRepository {
 
 	@Override
 	public List<Label> findAll() {
-		return null;
+		String sql = "SELECT `id`, `name`, `color`, `background`, `description` " +
+			"FROM `label`";
+
+		return Optional.of(template.query(sql, labelRowMapper())).orElse(Collections.emptyList());
 	}
 
-	@Override
-	public Label findById(Long id) {
-		return null;
+	private RowMapper<Label> labelRowMapper() {
+		return (rs, rowNum) -> {
+			Long id = rs.getLong("id");
+			String name = rs.getString("name");
+			String color = rs.getString("color");
+			String background = rs.getString("background");
+			String description = rs.getString("description");
+
+			return new Label(id, name, color, background, description);
+		};
 	}
 
 	@Override
