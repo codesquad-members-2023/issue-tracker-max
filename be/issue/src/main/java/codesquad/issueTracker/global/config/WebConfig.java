067bdeaa322f159c415394abe7f940554b0dd665
DOEKYONG <apochmn@naver.com>
2023-08-02 16:58:10 +0900

    #15 feat : jwt filter 구현

---
+++ be/issue/src/main/java/codesquad/issueTracker/global/config/WebConfig.java
@@ -0,0 +1,22 @@
+package codesquad.issueTracker.global.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import codesquad.issueTracker.global.filter.AuthFilter;
+import codesquad.issueTracker.jwt.service.JwtProvider;
+
+@Configuration
+public class WebConfig {
+
+	@Bean
+	public FilterRegistrationBean authFilter(JwtProvider provider, ObjectMapper mapper) {
+		FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+		filterRegistrationBean.setFilter(new AuthFilter(mapper, provider));
+		filterRegistrationBean.setOrder(1);
+		return filterRegistrationBean;
+	}
+}
