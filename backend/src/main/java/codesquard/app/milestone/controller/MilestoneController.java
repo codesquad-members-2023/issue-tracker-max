56c349d8ef4a3e3f445b639f68ee3af238fbea99
Sully <2dcoder@naver.com>
2023-08-02 17:24:46 +0900

    :sparkles:feat #36: 마일스톤 READ 기능 Controller 단계 구현

--- backend/src/main/java/codesquard/app/milestone/controller/MilestoneController.java
+++ backend/src/main/java/codesquard/app/milestone/controller/MilestoneController.java
@@ -5,18 +5,24 @@ import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import codesquard.app.milestone.dto.request.MilestoneSaveRequest;
+import codesquard.app.milestone.dto.request.MilestoneStatusRequest;
 import codesquard.app.milestone.dto.request.MilestoneUpdateRequest;
 import codesquard.app.milestone.dto.response.MilestoneDeleteResponse;
+import codesquard.app.milestone.dto.response.MilestoneReadResponse;
 import codesquard.app.milestone.dto.response.MilestoneSaveResponse;
+import codesquard.app.milestone.dto.response.MilestoneStatusUpdateResponse;
 import codesquard.app.milestone.dto.response.MilestoneUpdateResponse;
-import codesquard.app.milestone.dto.response.MilestoneSaveResponse;
+import codesquard.app.milestone.entity.MilestoneStatus;
 import codesquard.app.milestone.service.MilestoneService;
 
 @RestController
@@ -27,11 +33,34 @@ public class MilestoneController {
 		this.milestoneService = milestoneService;
 	}
 
+	@GetMapping("/api/milestones")
+	public ResponseEntity<MilestoneReadResponse> getOpened(
+		@RequestParam(name = "state", defaultValue = "opened") String openedString,
+		@RequestParam(name = "state", defaultValue = "closed") String closedString) {
+		MilestoneReadResponse milestoneReadResponse = milestoneService.makeMilestoneResponse(
+			chooseStatus(closedString));
+
+		return ResponseEntity.status(HttpStatus.OK)
+			.body(milestoneReadResponse.success());
+	}
+
+	/**
+	 * closed로 받아온 값이 closed가 아니라면 무조건 opened라는 의미이므로
+	 * 사실상 매개변수에 opened를 추가할 필요는 없음
+	 */
+	private MilestoneStatus chooseStatus(String closedString) {
+		if (closedString.equalsIgnoreCase("closed")) {
+			return MilestoneStatus.CLOSED;
+		}
+
+		return MilestoneStatus.OPENED;
+	}
+
 	@PostMapping("/api/milestones")
 	public ResponseEntity<MilestoneSaveResponse> save(@Valid @RequestBody MilestoneSaveRequest milestoneSaveRequest) {
 		Long milestoneId = milestoneService.saveMilestone(milestoneSaveRequest);
 		return ResponseEntity.status(HttpStatus.CREATED)
-			.body(MilestoneSaveResponse.success(true, milestoneId));
+			.body(MilestoneSaveResponse.success(milestoneId));
 	}
 
 	@PutMapping("/api/milestones/{milestoneId}")
@@ -40,7 +69,16 @@ public class MilestoneController {
 		milestoneService.updateMilestone(milestoneId, milestoneUpdateRequest);
 
 		return ResponseEntity.status(HttpStatus.OK)
-			.body(MilestoneUpdateResponse.success(true));
+			.body(MilestoneUpdateResponse.success());
+	}
+
+	@PatchMapping("/api/milestones/{milestoneId}/status")
+	public ResponseEntity<MilestoneStatusUpdateResponse> updateStatus(@PathVariable final Long milestoneId,
+		@Valid @RequestBody MilestoneStatusRequest milestoneStatusRequest) {
+		milestoneService.updateMilestoneStatus(milestoneId, milestoneStatusRequest);
+
+		return ResponseEntity.status(HttpStatus.OK)
+			.body(MilestoneStatusUpdateResponse.success());
 	}
 
 	@DeleteMapping("/api/milestones/{milestoneId}")
@@ -48,6 +86,6 @@ public class MilestoneController {
 		milestoneService.deleteMilestone(milestoneId);
 
 		return ResponseEntity.status(HttpStatus.OK)
-			.body(MilestoneDeleteResponse.success(true));
+			.body(MilestoneDeleteResponse.success());
 	}
 }
