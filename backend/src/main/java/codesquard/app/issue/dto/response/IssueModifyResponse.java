7a295a6f11bf29f583448b41f8ad6bf6ad58200a
JIHYE <jdh324@naver.com>
2023-08-03 02:06:29 +0900

    ♻refactor #42: getter 삭제 및 JsonProperty 추가

--- backend/src/main/java/codesquard/app/issue/dto/response/IssueModifyResponse.java
+++ backend/src/main/java/codesquard/app/issue/dto/response/IssueModifyResponse.java
@@ -1,13 +1,16 @@
 package codesquard.app.issue.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class IssueModifyResponse {
 
+	@JsonProperty("success")
 	private final boolean success;
 
-	public boolean isSuccess() {
-		return success;
+	public static IssueModifyResponse success() {
+		return new IssueModifyResponse(true);
 	}
 }
--- backend/src/main/java/codesquard/app/issue/dto/response/IssueSaveResponse.java
+++ backend/src/main/java/codesquard/app/issue/dto/response/IssueSaveResponse.java
@@ -1,18 +1,18 @@
 package codesquard.app.issue.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class IssueSaveResponse {
 
+	@JsonProperty("success")
 	private final boolean success;
+	@JsonProperty("id")
 	private final Long id;
 
-	public boolean isSuccess() {
-		return success;
-	}
-
-	public Long getId() {
-		return id;
+	public static IssueSaveResponse success(Long id) {
+		return new IssueSaveResponse(true, id);
 	}
 }
