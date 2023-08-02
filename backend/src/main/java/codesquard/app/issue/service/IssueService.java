840eaddf0e71dc46eda5e99f18a8a5e10294bf7c
JIHYE <jdh324@naver.com>
2023-08-02 15:05:02 +0900

    ✨feat #47: 이슈 수정 Service 구현

--- backend/src/main/java/codesquard/app/issue/service/IssueService.java
+++ backend/src/main/java/codesquard/app/issue/service/IssueService.java
@@ -5,8 +5,21 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import codesquard.app.errors.errorcode.IssueErrorCode;
+import codesquard.app.errors.exception.IllegalIssueStatusException;
+import codesquard.app.errors.exception.NoSuchIssueException;
+import codesquard.app.issue.dto.request.IssueModifyAssigneesRequest;
+import codesquard.app.issue.dto.request.IssueModifyContentRequest;
+import codesquard.app.issue.dto.request.IssueModifyLabelsRequest;
+import codesquard.app.issue.dto.request.IssueModifyMilestoneRequest;
+import codesquard.app.issue.dto.request.IssueModifyStatusRequest;
+import codesquard.app.issue.dto.request.IssueModifyTitleRequest;
 import codesquard.app.issue.dto.request.IssueSaveRequest;
+import codesquard.app.issue.entity.Issue;
+import codesquard.app.issue.entity.IssueStatus;
 import codesquard.app.issue.repository.IssueRepository;
+import codesquard.app.label.entity.Label;
+import codesquard.app.user.entity.User;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -32,6 +45,72 @@ public class IssueService {
 	private void registerIssueAssignee(Long issueId, List<Long> assignees) {
 		for (Long assignee : assignees) {
 			issueRepository.saveIssueAssignee(issueId, assignee);
+		}
+	}
+
+	@Transactional
+	public void modifyStatus(IssueModifyStatusRequest issueModifyStatusRequest, Long issueId) {
+		existIssue(issueId);
+		IssueStatus issueStatus;
+		try {
+			issueStatus = IssueStatus.valueOf(issueModifyStatusRequest.getStatus());
+		} catch (IllegalArgumentException e) {
+			throw new IllegalIssueStatusException(IssueErrorCode.INVALID_ISSUE_STATUS);
+		}
+		issueRepository.modifyStatus(issueStatus.name(), issueId);
+	}
+
+	@Transactional
+	public void modifyTitle(IssueModifyTitleRequest issueModifyTitleRequest, Long issueId) {
+		existIssue(issueId);
+		issueRepository.modifyTitle(issueModifyTitleRequest.getTitle(), issueId);
+	}
+
+	@Transactional
+	public void modifyContent(IssueModifyContentRequest issueModifyTitleRequest, Long issueId) {
+		existIssue(issueId);
+		issueRepository.modifyContent(issueModifyTitleRequest.getContent(), issueId);
+	}
+
+	@Transactional
+	public void modifyAssignees(IssueModifyAssigneesRequest issueModifyAssigneesRequest, Long issueId) {
+		existIssue(issueId);
+		issueRepository.deleteAssigneesById(issueId);
+		registerIssueAssignee(issueId, issueModifyAssigneesRequest.getAssignees());
+	}
+
+	@Transactional
+	public void modifyMilestone(IssueModifyMilestoneRequest issueModifyMilestoneRequest, Long issueId) {
+		existIssue(issueId);
+		issueRepository.modifyMilestone(issueModifyMilestoneRequest.getMilestone(), issueId);
+	}
+
+	@Transactional
+	public void modifyLabels(IssueModifyLabelsRequest issueModifyLabelsRequest, Long issueId) {
+		existIssue(issueId);
+		issueRepository.deleteLabelsById(issueId);
+		registerIssueLabel(issueId, issueModifyLabelsRequest.getLabels());
+	}
+
+	@Transactional(readOnly = true)
+	public Issue findById(Long issueId) {
+		return issueRepository.findById(issueId);
+	}
+
+	@Transactional(readOnly = true)
+	public List<User> findAssigneesById(Long issueId) {
+		return issueRepository.findAssigneesById(issueId);
+	}
+
+	@Transactional(readOnly = true)
+	public List<Label> findLabelsById(Long issueId) {
+		return issueRepository.findLabelsById(issueId);
+	}
+
+	@Transactional
+	private void existIssue(Long issueId) {
+		if (!issueRepository.exist(issueId)) {
+			throw new NoSuchIssueException(IssueErrorCode.NOT_FOUND_ISSUE);
 		}
 	}
 }
