c25262a2e48c66882c6e9127a89aecc90f2eaf58
O_jeegu <lumirite0@gmail.com>
2023-08-03 12:05:11 +0900

    feat #30: MethodArgumentNotValidException Handler 추가

--- be/src/main/java/com/issuetrackermax/common/exception/GlobalExceptionHandler.java
+++ be/src/main/java/com/issuetrackermax/common/exception/GlobalExceptionHandler.java
@@ -1,5 +1,9 @@
 package com.issuetrackermax.common.exception;
 
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -8,6 +12,17 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(ApiException.class)
 	public ErrorResponse apiExceptionHandler(ApiException e) {
 		ErrorCode errorCode = new ErrorCode(e.getHttpStatus().value(), e.getMessage());
+		return ErrorResponse.exception(errorCode);
+	}
+
+	public ErrorResponse validationExceptionHandler(MethodArgumentNotValidException e) {
+		String message = e.getBindingResult()
+			.getFieldErrors()
+			.stream()
+			.map(fieldError -> Optional.ofNullable(fieldError.getDefaultMessage()).orElse("유효하지 않은 값입니다."))
+			.findAny()
+			.orElseThrow();
+		ErrorCode errorCode = new ErrorCode(HttpStatus.BAD_REQUEST.value(), message);
 		return ErrorResponse.exception(errorCode);
 	}
 }
