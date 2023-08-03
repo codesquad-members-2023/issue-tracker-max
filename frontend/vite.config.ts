677fde520ef37ee2d74494786a890f7e4bf77f8f
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-08-03 23:13:47 +0900

    📝docs #9: svgr 플러그인 추가

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,10 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), svgr],
     server: {
         watch: {
             usePolling: true,// 폴링은 주기적으로 파일 시스템을 체크하여 변경 사항을 감지하는 방식
