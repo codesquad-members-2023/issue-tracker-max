f345a89716e1ab745272fef43ebbebe1d57cc48e
Charlie <kdjin1215@Naver.com>
2023-08-03 14:41:33 +0900

    #52 feat: cors 설정파일 추가 (#54)

---
+++ be/src/main/java/com/codesquad/issuetracker/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.codesquad.issuetracker.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://issue-tracker-team3.s3-website.ap-northeast-2.amazonaws.com")
+            .allowedOrigins("http://localhost:5173")
+            .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.HEAD.name(),
+                HttpMethod.PUT.name(), HttpMethod.DELETE.name(), HttpMethod.OPTIONS.name(),
+                HttpMethod.PATCH.name());
+    }
+
+}
