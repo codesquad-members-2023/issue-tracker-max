f0f8517b354a50fb342a74bfb8259e921db4caa4
othertkfka <57559288+othertkfka@users.noreply.github.com>
2023-08-03 18:41:00 +0900

    [BE] refactor: 담당자, 레이블, 마일스톤이 없을 경우 이슈 생성 로직 수정 (#41)

--- be/issue-tracker/src/main/java/org/presents/issuetracker/issue/service/IssueService.java
+++ be/issue-tracker/src/main/java/org/presents/issuetracker/issue/service/IssueService.java
@@ -32,40 +32,31 @@ public class IssueService {
 				.build()
 		);
 
-		//TODO: 만약 assignee가 지정이 안된다면 assignee:null인지 아니면 아예 assignee라는 key가 없는지
-		if (issueCreateRequestDto.getAssigneeIds() != null) {
-			issueRepository.deleteAllAssignee(savedIssueId);
-			issueRepository.addAssignee(
-				issueCreateRequestDto.getAssigneeIds().stream()
-					.map(assigneeId ->
-						Assignee.builder()
-							.issueId(savedIssueId)
-							.userId(assigneeId)
-							.build()
-					)
-					.collect(Collectors.toList())
-			);
-		}
+		issueRepository.addAssignee(
+			issueCreateRequestDto.getAssigneeIds().stream()
+				.map(assigneeId ->
+					Assignee.builder()
+						.issueId(savedIssueId)
+						.userId(assigneeId)
+						.build()
+				)
+				.collect(Collectors.toList())
+		);
 
-		if (issueCreateRequestDto.getLabelIds() != null) {
-			issueRepository.deleteAllLabel(savedIssueId);
-			issueRepository.addLabel(
-				issueCreateRequestDto.getLabelIds().stream()
-					.map(labelId ->
-						IssueLabel.builder()
-							.issueId(savedIssueId)
-							.labelId(labelId)
-							.build()
-					)
-					.collect(Collectors.toList())
-			);
-		}
+		issueRepository.addLabel(
+			issueCreateRequestDto.getLabelIds().stream()
+				.map(labelId ->
+					IssueLabel.builder()
+						.issueId(savedIssueId)
+						.labelId(labelId)
+						.build()
+				)
+				.collect(Collectors.toList())
+		);
 
-		if (issueCreateRequestDto.getMilestoneId() != null) {
-			issueRepository.setMilestone(
-				savedIssueId, issueCreateRequestDto.getMilestoneId()
-			);
-		}
+		issueRepository.setMilestone(
+			savedIssueId, issueCreateRequestDto.getMilestoneId()
+		);
 
 		return savedIssueId;
 	}
