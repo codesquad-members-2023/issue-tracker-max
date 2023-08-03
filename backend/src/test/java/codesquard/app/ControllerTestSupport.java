ff7df856094d85fb2a7d0f047c194800456a83c2
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-08-03 11:47:41 +0900

    ✅test #33: JwtProvider 객체 테스트 코드 추가

--- backend/src/test/java/codesquard/app/ControllerTestSupport.java
+++ backend/src/test/java/codesquard/app/ControllerTestSupport.java
@@ -3,6 +3,7 @@ package codesquard.app;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -11,6 +12,7 @@ import codesquard.app.comment.controller.CommentController;
 import codesquard.app.comment.service.CommentService;
 import codesquard.app.issue.controller.IssueController;
 import codesquard.app.issue.service.IssueService;
+import codesquard.app.jwt.JwtProvider;
 import codesquard.app.label.controller.LabelController;
 import codesquard.app.label.service.LabelService;
 import codesquard.app.user.controller.UserRestController;
@@ -22,18 +24,24 @@ import codesquard.app.user.service.UserService;
 	IssueController.class,
 	LabelController.class
 })
+@Import(value = {JwtProvider.class})
 public abstract class ControllerTestSupport {
 
 	@Autowired
 	protected MockMvc mockMvc;
 
 	@Autowired
+	protected JwtProvider jwtProvider;
+
+	@Autowired
 	protected ObjectMapper objectMapper;
 
 	@MockBean
 	protected IssueService issueService;
+
 	@MockBean
 	protected CommentService commentService;
+
 	@MockBean
 	protected LabelService labelService;
 
