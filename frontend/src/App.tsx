c865e711ceafe111c2ce77b9f00dfe24f108e9f2
Cat Hanbit <saejinpark951210@gmail.com>
2023-08-03 13:59:18 +0900

    리베이스전 임시커밋

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,45 +1,76 @@
-import { useState } from 'react';
-import { styled, ThemeProvider } from 'styled-components';
+import React, { useContext, useEffect, useState } from 'react';
+import { ThemeProvider } from 'styled-components';
 import { lightTheme } from './theme';
 import { darkTheme } from './theme';
 import GlobalStyle from './style/Global';
-import Button from './components/common/button/BaseButton';
-import ButtonLarge from './components/common/button/ButtonLarge';
-import ButtonSmall from './components/common/button/ButtonSmall';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  useLocation,
+} from 'react-router-dom';
+import Components from './pages/Components';
+import Login from './pages/Login';
+import Main from './pages/Main';
+
+import LogoDarkLarge from './asset/logo/logo_dark_large.svg';
+import LogoDarkMedium from './asset/logo/logo_dark_medium.svg';
+import LogoLightLarge from './asset/logo/logo_light_large.svg';
+import LogoLightMedium from './asset/logo/logo_light_medium.svg';
+import { AppContext } from './main';
+
+const AuthenticatedRoute = ({ children }: { children: React.ReactNode }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    const session = localStorage.getItem('session');
+    setIsAuthenticated(!!session);
+  }, [location]); // location이 바뀔 때마다 로그인 상태를 확인합니다.
+
+  return isAuthenticated ? children : <Navigate to="/login" replace />;
+};
 
 function App() {
   const [isLight, setIsLight] = useState<boolean>(true);
-
-  const changeTheme = () => {
+  const context = useContext(AppContext);
+  context.util.getLogoByTheme = () =>
+    isLight
+      ? {
+          large: LogoLightLarge,
+          medium: LogoLightMedium,
+        }
+      : {
+          large: LogoDarkLarge,
+          medium: LogoDarkMedium,
+        };
+  context.control.changeTheme = () => {
     setIsLight(!isLight);
   };
 
   return (
     <ThemeProvider theme={isLight ? lightTheme : darkTheme}>
       <GlobalStyle />
-      <StyledApp>
-        <ButtonLarge type="submit" iconName="plus">
-          BUTTON
-        </ButtonLarge>
-        <Button type="submit" outline iconName="plus">
-          BUTTON
-        </Button>
-        <Button type="submit" ghost flexible iconName="plus">
-          This is Flexible BUTTON
-        </Button>
-        <ButtonSmall type="submit" ghost iconName="plus">
-          BUTTON
-        </ButtonSmall>
-        <ButtonSmall type="button" ghost flexible onClick={changeTheme}>
-          Change Theme
-        </ButtonSmall>
-      </StyledApp>
+      {/* <ButtonSmall type="button" ghost flexible onClick={changeTheme}>
+        Change Theme
+      </ButtonSmall> */}
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <AuthenticatedRoute>
+                <Route element={<Main />} />
+              </AuthenticatedRoute>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/component" element={<Components />} />
+        </Routes>
+      </Router>
     </ThemeProvider>
   );
 }
-
-const StyledApp = styled.div`
-  background: ${({ theme }) => theme.color.neutral.surface.default};
-`;
 
 export default App;
--- frontend/src/style/Global.ts
+++ frontend/src/style/Global.ts
@@ -1,8 +1,13 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  * {
-    font-family: "Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
+  .blind {
+    width: 1px;
+    height: 1px;
+    margin: -1px;
+    position: absolute;
+    overflow: hidden;
+    clip-path: polygon(0 0, 0 0, 0 0, 0 0);
   }
 `;
 
