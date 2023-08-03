edf0df9976e3be97ef5e72848f0c01d1e32c90d4
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-08-03 17:12:24 +0900

    ✅test #33: 갱신토큰 갱신 서비스 테스트 코드 수정

--- backend/src/test/java/codesquard/app/user/service/AuthenticateUserServiceTest.java
+++ backend/src/test/java/codesquard/app/user/service/AuthenticateUserServiceTest.java
@@ -1,5 +1,7 @@
 package codesquard.app.user.service;
 
+import java.util.HashMap;
+
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,24 +11,42 @@ import org.springframework.transaction.annotation.Transactional;
 import codesquard.app.IntegrationTestSupport;
 import codesquard.app.authenticate_user.service.RefreshTokenServiceRequest;
 import codesquard.app.jwt.Jwt;
+import codesquard.app.jwt.JwtProvider;
+import codesquard.app.user.entity.AuthenticateUser;
+import codesquard.app.user.service.request.UserLoginServiceRequest;
+import codesquard.app.user.service.request.UserSaveServiceRequest;
 
 class AuthenticateUserServiceTest extends IntegrationTestSupport {
 
 	@Autowired
+	private JwtProvider jwtProvider;
+
+	@Autowired
 	private AuthenticateUserService authenticateUserService;
+
+	@Autowired
+	private UserService userService;
 
 	@Transactional
 	@Test
 	@DisplayName("refreshToken이 주어지고 토큰 갱신을 요청할때 토큰이 갱신된다")
 	public void refreshToken_givenRefreshToken_whenRefreshToken_thenResponseJwt() {
 		// given
-		String refreshToken = "eyJhbGciOiJIUzM4NCJ9.eyJleHAiOjE2OTM2Mzk0Njl9.VTsTjBvNnS_D2xAj2uNwYlqQ9v5Re8tEQNuPuojTrud7Ybf7b1_wm9s1ypmhLbzS";
-		RefreshTokenServiceRequest refreshTokenServiceRequest = new RefreshTokenServiceRequest(refreshToken);
+		// 회원가입
+		userService.signUp(new UserSaveServiceRequest("hong1234", "hong1234@gmail.com", "hong1234", "hong1234", null));
+
+		// 인증 유저 및 jwt 생성
+		HashMap<String, Object> claims = new HashMap<>();
+		Jwt jwt = jwtProvider.createJwt(claims);
+		AuthenticateUser authenticateUser = userService.verifyUser(new UserLoginServiceRequest("hong1234", "hong1234"));
+		authenticateUserService.updateRefreshToken(authenticateUser, jwt);
+		RefreshTokenServiceRequest refreshTokenServiceRequest = new RefreshTokenServiceRequest(
+			jwt.createRefreshTokenCookie().getValue());
 		// when
-		Jwt jwt = authenticateUserService.refreshToken(refreshTokenServiceRequest);
+		Jwt refreshedJwt = authenticateUserService.refreshToken(refreshTokenServiceRequest);
 		// then
 		SoftAssertions.assertSoftly(softAssertions -> {
-			softAssertions.assertThat(jwt).isNotNull();
+			softAssertions.assertThat(refreshedJwt).isNotNull();
 			softAssertions.assertAll();
 		});
 	}
