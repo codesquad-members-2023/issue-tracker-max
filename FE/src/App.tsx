2a652303cae03c609756beb996422ab3de9d8ba5
litae <109706689+qkdflrgs@users.noreply.github.com>
2023-08-03 15:55:12 +0900

    [FE] feat/#50: 리액트 라우터 추가 (#51)
    * add/#50: 리액터 라우터 의존성 추가
    
    * feat/#50: 리액터 라우터 적용
    
    * docs/#50: gitignore에 vercel 추가

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,14 +1,23 @@
 import { useDarkMode } from "./hooks/useDarkMode.ts";
+import AddIssuePage from "./pages/AddIssuePage.tsx";
 import MainPage from "./pages/MainPage.tsx";
 import { lightMode, darkMode } from "./styles/DesignSystem.ts";
 import { ThemeProvider } from "styled-components";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header.tsx";
 
 function App() {
   const [theme, toggleTheme] = useDarkMode();
   const themeMode = theme === "light" ? lightMode : darkMode;
   return (
     <ThemeProvider theme={themeMode}>
-      <MainPage toggleTheme={toggleTheme} />
+      <BrowserRouter>
+        <Header toggleTheme={toggleTheme} />
+        <Routes>
+          <Route path="" element={<MainPage />}></Route>
+          <Route path="/new" element={<AddIssuePage />}></Route>
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
