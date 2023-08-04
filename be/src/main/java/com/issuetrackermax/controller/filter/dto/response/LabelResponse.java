69575b72627d590ecbcd5ee0d52816cfbaa65daf
JJONSOO <pjs970616@naver.com>
2023-08-04 19:15:01 +0900

    fix #37 : 에러 메세지를 ErrorResponse가 가지도록 수정

--- be/src/main/java/com/issuetrackermax/controller/filter/dto/response/LabelResponse.java
+++ be/src/main/java/com/issuetrackermax/controller/filter/dto/response/LabelResponse.java
@@ -12,23 +12,33 @@ public class LabelResponse {
 
 	private Long id;
 	private String title;
+	private String textColor;
+	private String backgroundColor;
 
 	@Builder
-	public LabelResponse(Long id, String title) {
+	public LabelResponse(Long id, String title, String textColor, String backgroundColor) {
 		this.id = id;
 		this.title = title;
+		this.textColor = textColor;
+		this.backgroundColor = backgroundColor;
 	}
 
-	public static List<LabelResponse> convertToLabelResponseList(String labelIds, String labelTitles) {
+	public static List<LabelResponse> convertToLabelResponseList(String labelIds, String labelTitles,
+		String labelTextColors, String labelBackgroundColors) {
 		if (labelIds == null) {
 			return null;
 		}
 		List<String> ids = List.of(labelIds.split(","));
 		List<String> titles = List.of(labelTitles.split(","));
+		List<String> textColors = List.of(labelTextColors.split(","));
+		List<String> backgroundColors = List.of(labelBackgroundColors.split(","));
+
 		return IntStream.range(0, ids.size())
 			.mapToObj(i -> LabelResponse.builder()
 				.id(Long.parseLong(ids.get(i)))
 				.title(titles.get(i))
+				.textColor(textColors.get(i))
+				.backgroundColor(backgroundColors.get(i))
 				.build())
 			.collect(Collectors.toList());
 	}
