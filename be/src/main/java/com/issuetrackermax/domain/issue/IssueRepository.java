fc4263430c2e3dfdbf41530cd2df0e48679db224
JJONSOO <pjs970616@naver.com>
2023-08-05 00:36:43 +0900

    fix #37 : LabelRepository 에서 Label ID만 가져오고 title, textColor, backgroundColor는 직접 찾도록 수정한다.

--- be/src/main/java/com/issuetrackermax/domain/issue/IssueRepository.java
+++ be/src/main/java/com/issuetrackermax/domain/issue/IssueRepository.java
@@ -26,10 +26,7 @@ public class IssueRepository {
 
 	public IssueResultVO findIssueDetailsById(Long id) {
 		String sql = "SELECT i.id, i.is_open, i.title, "
-			+ "GROUP_CONCAT( l.id ORDER BY l.id SEPARATOR ',') AS label_ids, "
-			+ "GROUP_CONCAT( l.title ORDER BY l.id SEPARATOR ',') AS label_titles, "
-			+ "GROUP_CONCAT( l.text_color ORDER BY l.id SEPARATOR ',') AS label_text_colors,"
-			+ "GROUP_CONCAT( l.background_color ORDER BY l.id SEPARATOR ',') AS label_background_colors,"
+			+ "GROUP_CONCAT(DISTINCT l.id ORDER BY l.id SEPARATOR ',') AS label_ids, "
 			+ "m.id AS writer_id, m.nick_name AS writer, "
 			+ "GROUP_CONCAT(DISTINCT m2.id ORDER BY a.id SEPARATOR ',') AS assignee_ids, "
 			+ "GROUP_CONCAT(DISTINCT m2.nick_name ORDER BY a.id SEPARATOR ',') "
@@ -148,9 +145,6 @@ public class IssueRepository {
 			.isOpen(rs.getBoolean("is_open"))
 			.title(rs.getString("title"))
 			.labelIds(rs.getString("label_ids"))
-			.labelTitles(rs.getString("label_titles"))
-			.labelTextColors(rs.getString("label_text_colors"))
-			.labelBackgroundColors(rs.getString("label_background_colors"))
 			.writerId(rs.getLong("writer_id"))
 			.writer(rs.getString("writer"))
 			.assigneeIds(rs.getString("assignee_ids"))
