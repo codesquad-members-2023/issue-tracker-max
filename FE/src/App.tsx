f1cf2cf8f5d2221c8e22045ec7f513847260189e
litae <109706689+qkdflrgs@users.noreply.github.com>
2023-08-04 11:36:45 +0900

    [FE] feat/#65: 로그인 페이지 UI 구현 (#66)
    * feat/#65: 로그인 페이지 구현
    
    * feat/#65: 로그인 페이지 라우터 적용

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -4,18 +4,22 @@ import MainPage from "./pages/MainPage.tsx";
 import { lightMode, darkMode } from "./styles/DesignSystem.ts";
 import { ThemeProvider } from "styled-components";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage.tsx";
 import Header from "./components/Header/Header.tsx";
 
 function App() {
   const [theme, toggleTheme] = useDarkMode();
   const themeMode = theme === "light" ? lightMode : darkMode;
+
   return (
     <ThemeProvider theme={themeMode}>
       <BrowserRouter>
         <Header toggleTheme={toggleTheme} />
         <Routes>
-          <Route path="" element={<MainPage />}></Route>
+          <Route path="/issues" element={<MainPage />}></Route>
+          <Route path="" element={<LoginPage />}></Route>
           <Route path="/new" element={<AddIssuePage />}></Route>
+          <Route path="/issues/:filter" element={<MainPage />}></Route>
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
