e948a2ee24600bbaf22b4893ec6393fab6ee4477
JeongYong Park <66981851+23Yong@users.noreply.github.com>
2023-08-04 11:48:31 +0900

    feat: CORS 설정 (#88)
    * feat: CORS 설정

--- backend/src/main/java/kr/codesquad/issuetracker/infrastructure/config/WebConfig.java
+++ backend/src/main/java/kr/codesquad/issuetracker/infrastructure/config/WebConfig.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import kr.codesquad.issuetracker.presentation.auth.AuthPrincipalArgumentResolver;
@@ -18,5 +19,12 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
 		resolvers.add(authPrincipalArgumentResolver);
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/api/**")
+			.allowedOrigins("http://issue-tracker-8.s3-website.ap-northeast-2.amazonaws.com")
+			.allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "DELETE", "PATCH");
 	}
 }
