a884bdfe7ec2d91d383bc85b8bc1b3f795ffd08a
hjsong <song6240@naver.com>
2023-08-07 12:15:57 +0900

    🐛fix : msw warning message 예외처리

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,22 +5,7 @@ import "./styles/index.css";
 
 if (process.env.NODE_ENV === "development") {
   worker.start({
-    onUnhandledRequest(req, print) {
-      const excludedRoutes = [
-        "/BugBusters.jpeg",
-        "chrome-extension://cofdbpoegempjloogbagkncekinflcnj/fonts/OpenSans_VariableFont_wdth_wght.ttf",
-      ];
-
-      const isExcluded = excludedRoutes.some((route) =>
-        req.url.pathname.includes(route),
-      );
-
-      if (isExcluded) {
-        return;
-      }
-
-      print.warning();
-    },
+    onUnhandledRequest: "bypass",
   });
 }
 
