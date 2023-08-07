741d550412071e2d655567adb94e2d7181cc89a4
fuse <massenger7@gmail.com>
2023-08-07 16:25:09 +0900

    #26 chore: axios 설치 및 인스턴스 생성

---
+++ frontend/src/api/axios.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+const BASE_URL = import.meta.env.DEV ? '' : import.meta.env.VITE_API_URL;
+
+export default axios.create({
+  baseURL: BASE_URL,
+});
+
+export const axiosPrivate = axios.create({
+  baseURL: BASE_URL,
+  headers: { 'Content-Type': 'application/json' },
+  withCredentials: true,
+});
