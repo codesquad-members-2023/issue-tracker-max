3c8888cd9ec87c796ce7c1f21f315889810d82f1
jsh3418 <albert5428@gmail.com>
2023-08-08 12:13:20 +0900

    [FE] refactor: path 수정 및 상수화

--- fe/src/routes.tsx
+++ fe/src/routes.tsx
@@ -7,6 +7,13 @@ import { IssueListPage } from '@pages/IssueListPage';
 // import { MileStoneListPage } from '@pages/MileStoneListPage';
 // import { SignPage } from '@pages/SignPage';
 import { NotFoundPage } from '@pages/NotFoundPage';
+import {
+  ADD_ISSUE_PAGE,
+  ISSUE_DETAIL_PAGE,
+  ISSUE_LIST_PAGE,
+  LABEL_LIST_PAGE,
+  MILESTONE_LIST_PAGE,
+} from 'constants/PATH';
 
 type Props = {
   currentTheme: ThemeType;
@@ -18,12 +25,15 @@ export const AppRoutes: React.FC<Props> = ({ currentTheme, toggleTheme }) => {
     <Routes>
       {/* <Route path="/sign" element={<SignPage />} /> */}
       <Route path="/" element={<Header {...{ currentTheme, toggleTheme }} />}>
-        <Route index element={<IssueListPage />} />
-        <Route path=":query" element={<IssueListPage />} />
-        <Route path="add" element={<AddIssuePage />} />
-        <Route path="issue/:id" element={<IssueDetailPage />} />
-        {/* <Route path="label" element={<LabelListPage />} /> */}
-        {/* <Route path="milestone" element={<MileStoneListPage />} /> */}
+        <Route path={ISSUE_LIST_PAGE} element={<IssueListPage />} />
+        <Route path={`${ISSUE_LIST_PAGE}/:query`} element={<IssueListPage />} />
+        <Route path={ADD_ISSUE_PAGE} element={<AddIssuePage />} />
+        <Route
+          path={`${ISSUE_DETAIL_PAGE}/:id`}
+          element={<IssueDetailPage />}
+        />
+        {/* <Route path={LABEL_LIST_PAGE} element={<LabelListPage />} /> */}
+        {/* <Route path={MILESTONE_LIST_PAGE} element={<MileStoneListPage />} /> */}
         <Route path="/*" element={<NotFoundPage />} />
       </Route>
     </Routes>
