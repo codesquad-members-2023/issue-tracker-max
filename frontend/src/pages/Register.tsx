882d478faf9ff094014f274350a958f10777bc49
fuse <massenger7@gmail.com>
2023-08-08 16:58:39 +0900

    #26 chore: axios baseURL 원래대로 수정

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -34,7 +34,6 @@ export default function Register() {
         {
           headers: {
             'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
           },
           withCredentials: true,
         }
