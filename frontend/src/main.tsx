9d36c6ff6618014f5b0a31b63d49bbf180fb3284
fuse <massenger7@gmail.com>
2023-08-08 14:13:53 +0900

    #26 chore: 안 쓰는 import 삭제

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { worker } from './mocks/browser';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthProvider.tsx';
 
 if (import.meta.env.DEV) {
