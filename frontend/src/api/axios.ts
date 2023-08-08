1f3fb1faf8a074ed6cc11166d519923f815c6407
fuse <massenger7@gmail.com>
2023-08-08 16:58:39 +0900

    #26 chore: axios baseURL 원래대로 수정

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,5 @@
 import axios from 'axios';
-const BASE_URL = import.meta.env.DEV
-  ? 'http://ec2-43-202-134-94.ap-northeast-2.compute.amazonaws.com:8080'
-  : import.meta.env.VITE_API_URL;
+const BASE_URL = import.meta.env.DEV ? '' : import.meta.env.VITE_API_URL;
 
 export default axios.create({
   baseURL: BASE_URL,
