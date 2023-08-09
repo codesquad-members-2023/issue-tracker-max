99866df7e8e706d4f5a59408900552166bc8c36a
othertkfka <57559288+othertkfka@users.noreply.github.com>
2023-08-09 22:28:40 +0900

    [BE] test: IssueController 테스트 코드 작성 (#42)

--- be/issue-tracker/src/main/java/org/presents/issuetracker/issue/dto/request/IssueUpdateRequest.java
+++ be/issue-tracker/src/main/java/org/presents/issuetracker/issue/dto/request/IssueUpdateRequest.java
@@ -1,8 +1,12 @@
 package org.presents.issuetracker.issue.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class IssueUpdateRequest {
 	private Long id;
 	private String title;
