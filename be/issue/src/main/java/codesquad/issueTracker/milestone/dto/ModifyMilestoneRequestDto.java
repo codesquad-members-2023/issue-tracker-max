c02c9b3a52f311252de77a5ff48e7adad454a046
DOEKYONG <apochmn@naver.com>
2023-08-10 00:03:44 +0900

    #44 refactor : 마일스톤 requestDto 이름예외, 클래스명변경 및 등록 수정 동시에 사용하도록 리팩토링

--- be/issue/src/main/java/codesquad/issueTracker/milestone/dto/ModifyMilestoneRequestDto.java
+++ be/issue/src/main/java/codesquad/issueTracker/milestone/dto/ModifyMilestoneRequestDto.java
@@ -2,26 +2,26 @@ package codesquad.issueTracker.milestone.dto;
 
 import java.time.LocalDate;
 
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import codesquad.issueTracker.milestone.domain.Milestone;
 import lombok.Getter;
 
 @Getter
-public class SaveMilestoneRequestDto {
-
+public class ModifyMilestoneRequestDto {
+	@NotNull(message = "이름을 필수로 입력해주세요")
 	private String name;
 	private String description;
 	@Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = "yyyy-mm-dd 형식으로 입력해주세요")
 	private String doneDate;
-	private Boolean isClosed;
 
-	public static Milestone toEntity(SaveMilestoneRequestDto request) {
+
+	public static Milestone toEntity(ModifyMilestoneRequestDto request) {
 		return Milestone.builder()
 			.name(request.getName())
 			.description(request.getDescription())
 			.doneDate(request.getDoneDate()!=null?LocalDate.parse(request.getDoneDate()):null)
-			.isClosed(request.getIsClosed())
 			.build();
 	}
 
