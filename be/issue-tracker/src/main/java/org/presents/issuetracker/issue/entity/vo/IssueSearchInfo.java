86c079b718304b5e3fd92b591510f44d4a8b428d
sudago <sudago22@gmail.com>
2023-08-09 17:58:38 +0900

    [BE] fix: 요구사항 변경에 따라 assignee 에 loginId 추가 (#90)

--- be/issue-tracker/src/main/java/org/presents/issuetracker/issue/entity/vo/IssueSearchInfo.java
+++ be/issue-tracker/src/main/java/org/presents/issuetracker/issue/entity/vo/IssueSearchInfo.java
@@ -2,11 +2,10 @@ package org.presents.issuetracker.issue.entity.vo;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
 
-import org.presents.issuetracker.issue.entity.AssigneeImage;
 import org.presents.issuetracker.label.entity.Label;
 import org.presents.issuetracker.milestone.entity.Milestone;
+import org.presents.issuetracker.user.entity.User;
 
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -19,28 +18,22 @@ public class IssueSearchInfo {
 	private Long id;
 	private String title;
 	private String authorLoginId;
-	private List<AssigneeImage> assigneeImages;
+	private List<User> assignees;
 	private List<Label> labels;
 	private Milestone milestone;
 	private LocalDateTime createdAt;
 	private String status;
 
 	@Builder
-	public IssueSearchInfo(Long id, String title, String authorLoginId, List<AssigneeImage> assigneeImages,
-		List<Label> labels, Milestone milestone, LocalDateTime createdAt, String status) {
+	public IssueSearchInfo(Long id, String title, String authorLoginId, List<User> assignees, List<Label> labels,
+		Milestone milestone, LocalDateTime createdAt, String status) {
 		this.id = id;
 		this.title = title;
 		this.authorLoginId = authorLoginId;
-		this.assigneeImages = assigneeImages;
+		this.assignees = assignees;
 		this.labels = labels;
 		this.milestone = milestone;
 		this.createdAt = createdAt;
 		this.status = status;
-	}
-
-	public List<String> getGeneratedAssigneeImages() {
-		return assigneeImages.stream()
-			.map(AssigneeImage::getImage)
-			.collect(Collectors.toUnmodifiableList());
 	}
 }
