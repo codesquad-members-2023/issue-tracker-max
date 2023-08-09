59cb5630e3044b408c758cd57cf456a4ad61ef40
JJONSOO <pjs970616@naver.com>
2023-08-09 18:29:34 +0900

    feat #61 : Assignee validator 추가

--- be/src/main/java/com/issuetrackermax/service/assignee/AssigneeService.java
+++ be/src/main/java/com/issuetrackermax/service/assignee/AssigneeService.java
@@ -1,35 +1,31 @@
 package com.issuetrackermax.service.assignee;
 
-import java.util.List;
-
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.issuetrackermax.common.exception.ApiException;
-import com.issuetrackermax.common.exception.domain.AssigneeException;
 import com.issuetrackermax.domain.assignee.AssigneeRepository;
+import com.issuetrackermax.domain.issue.IssueValidator;
+import com.issuetrackermax.domain.member.MemberValidator;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Service
 public class AssigneeService {
+	private final MemberValidator memberValidator;
+	private final IssueValidator issueValidator;
 	private final AssigneeRepository assigneeRepository;
 
 	@Transactional
 	public void applyAssigneesToIssue(Long issueId, Long memberId) {
-		int count = assigneeRepository.applyAssigneesToIssue(issueId, memberId);
-		if (count != 1) {
-			throw new ApiException(AssigneeException.NOT_FOUND_ASSIGNEE);
-		}
+		memberValidator.existById(memberId);
+		issueValidator.existById(issueId);
+		assigneeRepository.applyAssigneesToIssue(issueId, memberId);
 	}
 
+	@Transactional
 	public void deleteAppliedAssignees(Long issueId) {
 		assigneeRepository.deleteAppliedAssignees(issueId);
 	}
 
-	@Transactional(readOnly = true)
-	public Boolean existByIds(List<Long> ids) {
-		return assigneeRepository.existByIds(ids);
-	}
 }
