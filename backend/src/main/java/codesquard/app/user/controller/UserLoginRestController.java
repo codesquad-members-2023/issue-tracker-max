8ae385c59c3d49cf3073661cb7eff8543c5d0d71
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-08-10 13:23:32 +0900

    ♻ refactor #105: 컨트롤러에서 수행하던 로그인 기능을 서비스 레이어로 이동

--- backend/src/main/java/codesquard/app/user/controller/UserLoginRestController.java
+++ backend/src/main/java/codesquard/app/user/controller/UserLoginRestController.java
@@ -4,8 +4,6 @@ import static codesquard.app.api.response.ResponseMessage.*;
 import static org.springframework.http.HttpStatus.*;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,12 +19,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import codesquard.app.api.errors.errorcode.LoginErrorCode;
 import codesquard.app.api.errors.exception.RestApiException;
 import codesquard.app.api.response.ApiResponse;
+import codesquard.app.authenticate_user.controller.response.AuthenticateUserLoginResponse;
 import codesquard.app.authenticate_user.entity.AuthenticateUser;
-import codesquard.app.jwt.Jwt;
+import codesquard.app.authenticate_user.service.AuthenticateUserService;
+import codesquard.app.authenticate_user.service.response.AuthenticateUserLoginServiceResponse;
 import codesquard.app.jwt.JwtProvider;
 import codesquard.app.jwt.filter.VerifyUserFilter;
-import codesquard.app.user.controller.response.UserLoginResponse;
-import codesquard.app.user.service.AuthenticateUserService;
 
 @RequestMapping(path = "/api")
 @RestController
@@ -46,27 +44,21 @@ public class UserLoginRestController {
 	}
 
 	@PostMapping("/login")
-	public ApiResponse<UserLoginResponse> login(HttpServletRequest request, HttpServletResponse response) throws
+	public ApiResponse<AuthenticateUserLoginResponse> login(HttpServletRequest request,
+		HttpServletResponse response) throws
 		IOException {
 		Object attribute = request.getAttribute(VerifyUserFilter.AUTHENTICATE_USER);
 		// attribute가 AuthenticateUser 객체인지 검증
 		if (!(attribute instanceof AuthenticateUser)) {
 			throw new RestApiException(LoginErrorCode.NOT_MATCH_LOGIN);
 		}
-		Map<String, Object> claims = new HashMap<>();
-		AuthenticateUser authenticateUser = (AuthenticateUser)attribute;
-		// 1. AuthenticateUser 객체를 json으로 변환
-		String authenticateUserJson = objectMapper.writeValueAsString(authenticateUser);
-		// 2. key="authenticateUser" value=인증 객체의 json 데이터를 claims 맵에 저장
-		claims.put(VerifyUserFilter.AUTHENTICATE_USER, authenticateUserJson);
-		// 3. claims을 기반으로 JWT 객체(액세스 토큰, 갱신 토큰) 생성
-		Jwt jwt = jwtProvider.createJwt(claims);
-		// 4. 갱신 토큰 최신 업데이트
-		authenticateUserService.updateRefreshToken(authenticateUser, jwt);
-		// 5. Authroization 헤더에 액세스 토큰 저장
-		response.addHeader("Authorization", jwt.createAccessTokenHeaderValue());
-		// 6. 쿠키에 key=refreshToken, value=갱신토큰 값 저장
-		response.addCookie(jwt.createRefreshTokenCookie());
-		return ApiResponse.of(OK, USER_LOGIN_SUCCESS, new UserLoginResponse(authenticateUser, jwt));
+		AuthenticateUserLoginServiceResponse loginServiceResponse =
+			authenticateUserService.login((AuthenticateUser)attribute);
+
+		// Authroization 헤더에 액세스 토큰 저장
+		response.addHeader("Authorization", loginServiceResponse.createAccessTokenHeaderValue());
+		// 쿠키에 key=refreshToken, value=갱신토큰 값 저장
+		response.addCookie(loginServiceResponse.createRefreshTokenCookie());
+		return ApiResponse.of(OK, USER_LOGIN_SUCCESS, loginServiceResponse.toAuthenticateUserLoginResponse());
 	}
 }
