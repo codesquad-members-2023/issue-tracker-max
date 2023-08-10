6c63ce2fd3f4b5bb1f411c187edf69fb9fb97b03
litae <109706689+qkdflrgs@users.noreply.github.com>
2023-08-10 19:54:49 +0900

    [FE] feat/#91: 이슈 상세 페이지 구현 (#142)
    * refactor/#91: 사이드바 컴포넌트 리팩토링
    
    * feat/#91: 이슈 상세 페이지 구현
    
    * feat/#91: 이슈 상태 컴포넌트 구현
    
    * feat/#91: 라벨 인풋 컴포넌트 구현

---
+++ FE/src/components/common/TextInput/LabelInput.tsx
@@ -0,0 +1,70 @@
+import { styled } from "styled-components";
+import Input from "../Input/Input";
+
+type Props = {
+  icon?: string;
+  id: string;
+  label: string;
+  placeholder: string;
+  value?: string;
+  maxLength?: number;
+  onChange?(e: React.ChangeEvent<HTMLInputElement>): void;
+  onClick?(): void;
+};
+
+export default function LabelInput({
+  icon,
+  id,
+  label,
+  placeholder,
+  value,
+  maxLength,
+  onChange,
+  onClick,
+}: Props) {
+  return (
+    <Container>
+      <Label htmlFor={id}>{label}</Label>
+      <Input
+        id={id}
+        placeholder={placeholder}
+        value={value}
+        maxLength={maxLength}
+        onChange={onChange}
+      />
+      {icon && (
+        <RandomButton onClick={onClick}>
+          <IconImg src={`/icons/${icon}.svg`} />
+        </RandomButton>
+      )}
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  padding: 8px 16px;
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  width: 100%;
+  height: 40px;
+  background-color: ${({ theme }) => theme.colorSystem.neutral.surface.bold};
+  border-radius: ${({ theme }) => theme.radius.large};
+`;
+
+const Label = styled.label`
+  width: 64px;
+  font: ${({ theme }) => theme.font.displayMedium12};
+  color: ${({ theme }) => theme.colorSystem.neutral.text.weak};
+`;
+
+const RandomButton = styled.button`
+  width: 16px;
+  height: 16px;
+  background-color: transparent;
+`;
+
+const IconImg = styled.img`
+  width: 100%;
+  height: 100%;
+`;
