f0951198071083de8ef8cf154081b6d618726553
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-08-10 16:21:16 +0900

    🐛fix #153: allowOriginPatterns로 변경

--- backend/src/main/java/codesquard/app/config/WebConfig.java
+++ backend/src/main/java/codesquard/app/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("*")
+			.allowedOriginPatterns("*")
 			.allowedHeaders("*")
 			.allowCredentials(true)
 			.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
