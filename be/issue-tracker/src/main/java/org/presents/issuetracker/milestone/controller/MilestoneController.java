24a05ecc26fb0545b48f92f4e8419bf96694ad03
jsjseop <57559288+jsjseop@users.noreply.github.com>
2023-08-10 20:53:12 +0900

    [BE] feat: 마일스톤 선택용 목록 구현 (#72)

--- be/issue-tracker/src/main/java/org/presents/issuetracker/milestone/controller/MilestoneController.java
+++ be/issue-tracker/src/main/java/org/presents/issuetracker/milestone/controller/MilestoneController.java
@@ -1,7 +1,27 @@
 package org.presents.issuetracker.milestone.controller;
 
+import java.util.List;
+
+import org.presents.issuetracker.milestone.dto.response.MilestonePreviewResponse;
+import org.presents.issuetracker.milestone.service.MilestoneService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequestMapping("/milestones")
+@RequiredArgsConstructor
 public class MilestoneController {
+	private final MilestoneService milestoneService;
+
+	@GetMapping("/previews")
+	public ResponseEntity<List<MilestonePreviewResponse>> getMilestonePreviews() {
+		List<MilestonePreviewResponse> milestonePreviews = milestoneService.getMilestonePreviews();
+		// 필터링 뷰에서 '마일스톤이 없는 이슈'를 표시하기 위해서 0번째에 객체 추가
+		milestonePreviews.add(0, MilestonePreviewResponse.getMilestoneNotAssignedResponse());
+		return ResponseEntity.ok().body(milestonePreviews);
+	}
 }
--- be/issue-tracker/src/main/java/org/presents/issuetracker/milestone/dto/response/MilestonePreviewResponse.java
+++ be/issue-tracker/src/main/java/org/presents/issuetracker/milestone/dto/response/MilestonePreviewResponse.java
@@ -8,9 +8,23 @@ import lombok.Getter;
 @Getter
 @Builder
 public class MilestonePreviewResponse {
+	private static final Long NO_MILESTONE_ID = 0L;
+	private static final String NO_MILESTONE_NAME = "none";
+	private static final int NO_MILESTONE_PROGRESS = 0;
+
 	private Long id;
 	private String name;
 	private int progress;
+
+	private static final MilestonePreviewResponse MILESTONE_NOT_ASSIGNED_RESPONSE = MilestonePreviewResponse.builder()
+		.id(NO_MILESTONE_ID)
+		.name(NO_MILESTONE_NAME)
+		.progress(NO_MILESTONE_PROGRESS)
+		.build();
+
+	public static MilestonePreviewResponse getMilestoneNotAssignedResponse() {
+		return MILESTONE_NOT_ASSIGNED_RESPONSE;
+	}
 
 	public static MilestonePreviewResponse from(MilestonePreview milestonePreview) {
 		if (milestonePreview == null) {
