7b7e565c672e99c6ff3e77b029f57759f0a749ad
JJONSOO <pjs970616@naver.com>
2023-08-11 12:10:44 +0900

    feat #78 : 같은 이름의 Issue 존재시 예외를 발생시키는 테스트.

--- be/src/test/java/com/issuetrackermax/service/label/LabelServiceTest.java
+++ be/src/test/java/com/issuetrackermax/service/label/LabelServiceTest.java
@@ -10,6 +10,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.issuetrackermax.common.exception.ApiException;
+import com.issuetrackermax.common.exception.domain.LabelException;
 import com.issuetrackermax.controller.label.dto.request.LabelModifyRequest;
 import com.issuetrackermax.controller.label.dto.request.LabelPostRequest;
 import com.issuetrackermax.controller.label.dto.response.LabelDetailResponse;
@@ -76,6 +78,23 @@ class LabelServiceTest extends IntegrationTestSupport {
 			() -> assertThat(label.getBackgroundColor()).isEqualTo("0#2222")
 		);
 
+	}
+
+	@DisplayName("레이블을 저장하고,같은 이름의 레이블을 저장하면 오류가 발생한다..")
+	@Test
+	void saveDuplicateTitle() {
+		// given
+		LabelPostRequest labelPostRequest = new LabelPostRequest("title1", "description1", "0#1111", "0#2222");
+		Long id = labelService.save(labelPostRequest);
+		LabelPostRequest labelPostRequest2 = new LabelPostRequest("title1", "description1", "0#1111", "0#2222");
+
+		// when & then
+		assertThatThrownBy(() -> labelService.save(labelPostRequest2))
+			.isInstanceOf(ApiException.class)
+			.satisfies(exception -> {
+				ApiException apiException = (ApiException)exception;
+				assertThat(apiException.getCustomException()).isInstanceOf(LabelException.class);
+			});
 	}
 
 	@DisplayName("저장된 레이블을 수정하고, 수정된 값을 확인한다.")
