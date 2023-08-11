4b316dfad53fb3a42178de80aeaa256b04dc678a
O_jeegu <lumirite0@gmail.com>
2023-08-11 12:30:54 +0900

    feat #68: 회원가입 시 아이디 중복 체크 기능 추가

--- be/src/main/java/com/issuetrackermax/service/member/MemberService.java
+++ be/src/main/java/com/issuetrackermax/service/member/MemberService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.issuetrackermax.controller.auth.dto.response.MemberProfileResponse;
+import com.issuetrackermax.controller.member.dto.request.CheckLoginIdRequest;
 import com.issuetrackermax.controller.member.dto.request.SignUpRequest;
 import com.issuetrackermax.domain.member.MemberRepository;
 import com.issuetrackermax.domain.member.MemberValidator;
@@ -16,6 +17,10 @@ import lombok.RequiredArgsConstructor;
 public class MemberService {
 	private final MemberValidator memberValidator;
 	private final MemberRepository memberRepository;
+
+	public void checkLoginIdDuplication(CheckLoginIdRequest checkLoginIdRequest) {
+		memberValidator.existLoginId(checkLoginIdRequest.getLoginId());
+	}
 
 	@Transactional
 	public void registerMember(SignUpRequest signUpRequest) {
