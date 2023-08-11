13edeb9486b1ee0ca935ecd2f503f1ae800c599d
JJONSOO <pjs970616@naver.com>
2023-08-11 12:09:47 +0900

    feat #78 : 마일스톤 저장시 name을 title로 수정

--- be/src/main/java/com/issuetrackermax/controller/milestone/dto/request/MilestonePostRequest.java
+++ be/src/main/java/com/issuetrackermax/controller/milestone/dto/request/MilestonePostRequest.java
@@ -9,13 +9,13 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class MilestonePostRequest {
-	private String name;
+	private String title;
 	private LocalDateTime dueDate;
 	private String description;
 
 	@Builder
-	public MilestonePostRequest(String name, LocalDateTime dueDate, String description) {
-		this.name = name;
+	public MilestonePostRequest(String title, LocalDateTime dueDate, String description) {
+		this.title = title;
 		this.dueDate = dueDate;
 		this.description = description;
 	}
