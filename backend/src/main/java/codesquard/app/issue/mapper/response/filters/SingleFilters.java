180b51bcc722619a8fe4f6236860baba065a2021
Sully <2dcoder@naver.com>
2023-08-11 11:35:51 +0900

    ♻️refactor #127: List<String> -> String 타입 변경

--- backend/src/main/java/codesquard/app/issue/mapper/response/filters/SingleFilters.java
+++ backend/src/main/java/codesquard/app/issue/mapper/response/filters/SingleFilters.java
@@ -1,7 +1,5 @@
 package codesquard.app.issue.mapper.response.filters;
 
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AllArgsConstructor;
@@ -31,11 +29,6 @@ public class SingleFilters {
 		}
 	}
 
-	public static final String IS_OPENED = "is:opened";
-	public static final String NAME_OPENED = "열린 이슈";
-	public static final String IS_CLOSED = "is:closed";
-	public static final String NAME_CLOSED = "닫힌 이슈";
-
 	public enum ME {
 		AUTHOR("author:@me", "내가 작성한 이슈"),
 		ASSIGNEE("assignee:@me", "나에게 할당된 이슈"),
@@ -58,13 +51,6 @@ public class SingleFilters {
 		}
 	}
 
-	public static final String AUTHOR_ME = "author:@me";
-	public static final String NAME_AUTHOR = "내가 작성한 이슈";
-	public static final String ASSIGNEE_ME = "assignee:@me";
-	public static final String NAME_ASSIGNEE = "나에게 할당된 이슈";
-	public static final String MENTIONS_ME = "mentions:@me";
-	public static final String NAME_MENTIONS = "내가 댓글을 남긴 이슈";
-
 	@JsonProperty("id")
 	private Long id;
 	// "내가 댓글을 남긴 이슈"
@@ -72,7 +58,7 @@ public class SingleFilters {
 	private String name;
 	// ["is:closed", memtions:@me"]
 	@JsonProperty("conditions")
-	private List<String> conditions;
+	private String conditions;
 	// false
 	// conditions에 지정된 형식이 아니라면 false
 	// 지정된 형식이 하나라도 존재한다면 true
