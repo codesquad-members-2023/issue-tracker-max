d83bf36777a834c3e204ee7c7c5794f96da1efc1
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-08-12 01:07:58 +0900

    🐛fix #170: 설정 변경

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,12 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
-  server: {
-    watch: {
-      usePolling: true, // 폴링은 주기적으로 파일 시스템을 체크하여 변경 사항을 감지하는 방식
+    plugins: [react(), svgr()],
+    server: {
+        host: true, // needed for the Docker Container port mapping to work
+        port: 5173, // you can replace this port with any port
     },
-    host: true, // needed for the Docker Container port mapping to work
-    strictPort: true,
-    port: 5173, // you can replace this port with any port
-  },
 });
