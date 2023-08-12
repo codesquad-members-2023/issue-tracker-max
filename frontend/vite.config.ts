82b002f352d5a830fafd02c1a1a3723b787b0014
hjsong <song6240@naver.com>
2023-08-12 17:40:26 +0900

    🐛fix #178: vite 프록시 설정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,13 @@ import svgr from "vite-plugin-svgr";
 export default defineConfig({
   plugins: [react(), svgr()],
   server: {
+    proxy: {
+      "/api": {
+        target: "http://3.36.120.124:8080",
+        changeOrigin: true,
+        secure: false,
+      }
+    },
     watch: {
       usePolling: true, // 폴링은 주기적으로 파일 시스템을 체크하여 변경 사항을 감지하는 방식
     },
