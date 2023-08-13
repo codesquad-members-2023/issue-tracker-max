fcb624d23edb62f633ee321d80c25a65c44bf1fb
hyeseon <chunghye1998@gmail.com>
2023-08-14 01:18:03 +0900

    #47 refactor: userId 찾는 로직 Service -> Controller로 이동

--- be/issue/src/main/java/codesquad/issueTracker/comment/service/CommentService.java
+++ be/issue/src/main/java/codesquad/issueTracker/comment/service/CommentService.java
@@ -23,8 +23,7 @@ public class CommentService {
     }
 
     @Transactional
-    public Long save(HttpServletRequest request, Long issueId, CommentRequestDto commentRequestDto) {
-        Long userId = Long.parseLong(String.valueOf(request.getAttribute("userId")));
+    public Long save(Long userId, Long issueId, CommentRequestDto commentRequestDto) {
         return commentRepository.create(userId, issueId, commentRequestDto)
                 .orElseThrow(() -> new CustomException(ErrorCode.DB_EXCEPTION));
     }
