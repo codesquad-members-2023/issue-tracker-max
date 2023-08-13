129e38dc71b0cd9ce216049909665aabb66d0aa3
DOEKYONG <apochmn@naver.com>
2023-08-13 14:50:03 +0900

    #44 fix : 기본생성자가 없어서 역직렬화가 안되어서 요청이 실패했던 버그 수정

--- be/issue/src/main/java/codesquad/issueTracker/milestone/dto/MileStoneStatusDto.java
+++ be/issue/src/main/java/codesquad/issueTracker/milestone/dto/MileStoneStatusDto.java
@@ -2,9 +2,11 @@ package codesquad.issueTracker.milestone.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class MileStoneStatusDto {
 
 	private String status;
