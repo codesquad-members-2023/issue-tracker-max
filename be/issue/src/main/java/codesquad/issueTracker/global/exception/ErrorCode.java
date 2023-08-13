bdb31d43aeb2f75d976e2788c453cdc4a71ed350
hyeseon <chunghye1998@gmail.com>
2023-08-14 01:18:57 +0900

    #47 refactor: DB_EXCEPTION ErrorCode를 상황에 맞게 분리

--- be/issue/src/main/java/codesquad/issueTracker/global/exception/ErrorCode.java
+++ be/issue/src/main/java/codesquad/issueTracker/global/exception/ErrorCode.java
@@ -11,10 +11,8 @@ import io.jsonwebtoken.security.SignatureException;
 
 public enum ErrorCode implements StatusCode {
 
-	REQUEST_VALIDATION_FAIL(HttpStatus.BAD_REQUEST),
-
 	// -- [Common] -- ]
-	DB_EXCEPTION(HttpStatus.SERVICE_UNAVAILABLE, "DB 서버 오류"),
+	REQUEST_VALIDATION_FAIL(HttpStatus.BAD_REQUEST),
 
 	// -- [OAuth] -- //
 	NOT_SUPPORTED_PROVIDER(HttpStatus.BAD_REQUEST, "지원하지 않는 로그인 방식입니다."),
@@ -32,10 +30,14 @@ public enum ErrorCode implements StatusCode {
 	ALREADY_EXIST_USER(HttpStatus.BAD_REQUEST, "이미 존재하는 유저입니다."),
 	NOT_FOUND_USER(HttpStatus.BAD_REQUEST, "해당하는 유저가 없습니다."),
 	FAILED_LOGIN_USER(HttpStatus.BAD_REQUEST, "로그인에 실패했습니다. 아이디, 비밀번호를 다시 입력해주세요. "),
+	FAILED_LOGOUT_USER(HttpStatus.SERVICE_UNAVAILABLE, "DB 서버 오류로 인해 사용자 로그아웃에 실패했습니다."),
 
 	// -- [Comment] -- //
 	NOT_EXIST_COMMENT(HttpStatus.BAD_REQUEST, "존재하지 않는 댓글입니다."),
 	ALREADY_DELETED_COMMENT(HttpStatus.BAD_REQUEST, "이미 삭제된 댓글입니다."),
+	FAILED_CREATE_COMMENT(HttpStatus.SERVICE_UNAVAILABLE, "DB 서버 오류로 인해 댓글 생성에 실패했습니다."),
+	FAILED_UPDATE_COMMENT(HttpStatus.SERVICE_UNAVAILABLE, "DB 서버 오류로 인해 댓글 수정에 실패했습니다."),
+	FAILED_DELETE_COMMENT(HttpStatus.SERVICE_UNAVAILABLE,"DB 서버 오류로 인해 댓글 삭제에 실패했습니다."),
 
 	// -- [Milestone] -- //
 	INVALIDATE_DATE(HttpStatus.BAD_REQUEST, "현재 날짜보다 이전 날짜 입니다."),
@@ -44,13 +46,10 @@ public enum ErrorCode implements StatusCode {
 	ILLEGAL_STATUS_MILESTONE(HttpStatus.BAD_REQUEST, "올바르지 않은 상태 입력 입니다."),
 
 	// -- [Label] -- //
-
 	LABEL_INSERT_FAILED(HttpStatus.BAD_REQUEST, "DB에서 라벨 생성에 실패했습니다."),
 	LABEL_UPDATE_FAILED(HttpStatus.BAD_REQUEST, "DB에서 라벨 수정에 실패했습니다."),
 	LABEL_DELETE_FAILED(HttpStatus.BAD_REQUEST, "DB에서 라벨 삭제에 실패했습니다"),
 	LABEL_FIND_FAILED(HttpStatus.BAD_REQUEST, "서버 오류로 라벨을 조회할 수 없습니다");
-
-
 
 
 	private HttpStatus status;
