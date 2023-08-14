f092be106d80c751bd849e857e4cf96173654f42
yhpark95 <98851575+yhpark95@users.noreply.github.com>
2023-08-14 15:09:58 +0900

    feat #63/ Comment 추가, 수정 api 추가

--- be/src/main/java/com/issuetrackermax/controller/comment/CommentController.java
+++ be/src/main/java/com/issuetrackermax/controller/comment/CommentController.java
@@ -1,15 +1,43 @@
 package com.issuetrackermax.controller.comment;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.issuetrackermax.controller.ApiResponse;
+import com.issuetrackermax.controller.comment.dto.request.CommentCreateRequest;
+import com.issuetrackermax.controller.comment.dto.request.CommentModifyRequest;
+import com.issuetrackermax.controller.comment.dto.response.CommentResponse;
 import com.issuetrackermax.service.comment.CommentService;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/issues/{id}/comments")
 public class CommentController {
+	private static final String MEMBER_ID = "memberId";
 	private final CommentService commentService;
+
+	@PatchMapping("/{id}")
+	public ApiResponse<Void> modifyComment(@PathVariable Long id,
+		@RequestBody CommentModifyRequest commentModifyRequest,
+		HttpServletRequest request) {
+		Integer memberId = (Integer)request.getAttribute(MEMBER_ID);
+		commentService.modifyComment(commentModifyRequest, id, memberId.longValue());
+		return ApiResponse.success();
+	}
+
+	@PostMapping
+	public ApiResponse<CommentResponse> post(@RequestBody CommentCreateRequest commentCreateRequest,
+		HttpServletRequest request) {
+		Integer memberId = (Integer)request.getAttribute(MEMBER_ID);
+		CommentResponse commentResponse = commentService.postComment(commentCreateRequest, memberId.longValue());
+		return ApiResponse.success(commentResponse);
+	}
 }
