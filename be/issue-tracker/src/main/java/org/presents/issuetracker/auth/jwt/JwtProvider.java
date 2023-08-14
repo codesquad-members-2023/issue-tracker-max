8c68b7a366b244ed9975e73a2ffcfe5460165139
jsjseop <57559288+jsjseop@users.noreply.github.com>
2023-08-14 16:01:53 +0900

    [BE] refactor: secretkey 생성자에서 인코딩 하도록 변경 (#114)

--- be/issue-tracker/src/main/java/org/presents/issuetracker/auth/jwt/JwtProvider.java
+++ be/issue-tracker/src/main/java/org/presents/issuetracker/auth/jwt/JwtProvider.java
@@ -1,10 +1,11 @@
 package org.presents.issuetracker.auth.jwt;
 
+import java.security.Key;
 import java.util.Base64;
 import java.util.Date;
 import java.util.Map;
 
-import javax.crypto.SecretKey;
+import javax.annotation.PostConstruct;
 
 import org.presents.issuetracker.global.util.JsonUtil;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,21 +18,15 @@ import io.jsonwebtoken.security.Keys;
 @Component
 public class JwtProvider {
 
-	private SecretKey cachedSecretKey;
-
 	@Value("${jwt.secret}")
 	private String secretKeyPlain;
 
-	private SecretKey encodeSecretKey() {
-		String keyBased64Encoded = Base64.getEncoder().encodeToString(secretKeyPlain.getBytes());
-		return Keys.hmacShaKeyFor(keyBased64Encoded.getBytes());
-	}
+	private Key key;
 
-	public SecretKey getSecretKey() {
-		if (cachedSecretKey == null) {
-			cachedSecretKey = encodeSecretKey();
-		}
-		return cachedSecretKey;
+	@PostConstruct
+	protected void init() {
+		String keyBased64Encoded = Base64.getEncoder().encodeToString(secretKeyPlain.getBytes());
+		key = Keys.hmacShaKeyFor(keyBased64Encoded.getBytes());
 	}
 
 	public String createToken(Map<String, Object> claims, int seconds) {
@@ -41,26 +36,26 @@ public class JwtProvider {
 		return Jwts.builder()
 			.claim("body", JsonUtil.toStr(claims))
 			.setExpiration(expiration)
-			.signWith(getSecretKey(), SignatureAlgorithm.HS512)
+			.signWith(key, SignatureAlgorithm.HS512)
 			.compact();
 	}
 
 	public boolean verify(String token) {
 		try {
 			Jwts.parserBuilder()
-				.setSigningKey(getSecretKey())
+				.setSigningKey(key)
 				.build()
 				.parseClaimsJws(token);
+			return true;
 		} catch (Exception e) {
 			e.printStackTrace();
 			return false;
 		}
-		return true;
 	}
 
 	public Map<String, Object> getClaims(String token) {
 		String claims = Jwts.parserBuilder()
-			.setSigningKey(getSecretKey())
+			.setSigningKey(key)
 			.build()
 			.parseClaimsJws(token)
 			.getBody()
