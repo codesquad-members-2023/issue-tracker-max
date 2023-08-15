01daf1ae59cb20300c0cc6281e6ca1a791fa4eaf
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-08-15 22:23:40 +0900

    ✅test #210: UserRestController 샘플 데이터를 FixedUserFactory 객체를 이용하여 생성

--- backend/src/test/java/codesquard/app/user/fixture/FixedUserFactory.java
+++ backend/src/test/java/codesquard/app/user/fixture/FixedUserFactory.java
@@ -1,8 +1,10 @@
 package codesquard.app.user.fixture;
 
+import codesquard.app.user.controller.request.UserSaveRequest;
 import codesquard.app.user.entity.User;
 import codesquard.app.user.service.request.UserLoginServiceRequest;
 import codesquard.app.user.service.request.UserSaveServiceRequest;
+import codesquard.app.user.service.response.UserSaveServiceResponse;
 
 public class FixedUserFactory {
 
@@ -15,15 +17,48 @@ public class FixedUserFactory {
 		return new User(null, LOGIN_ID, EMAIL, PASSWORD, null);
 	}
 
+	public static UserSaveRequest userSaveRequest() {
+		return new UserSaveRequest(LOGIN_ID, EMAIL, PASSWORD, PASSWORD);
+	}
+
+	public static UserSaveRequest emptyLoginIdUserSaveRequest() {
+		return new UserSaveRequest("", EMAIL, PASSWORD, PASSWORD);
+	}
+
 	public static UserSaveServiceRequest userSaveServiceRequest() {
 		return new UserSaveServiceRequest(LOGIN_ID, EMAIL, PASSWORD, PASSWORD, null);
+	}
+
+	public static UserSaveRequest invalidEmailUserSaveRequest(String email) {
+		return new UserSaveRequest(LOGIN_ID, email, PASSWORD, PASSWORD);
+	}
+
+	public static UserSaveRequest invalidPasswordUserSaveRequest(String password) {
+		return new UserSaveRequest(LOGIN_ID, EMAIL, password, PASSWORD);
+	}
+
+	public static UserSaveRequest invalidPasswordAndPasswordConfirmUserSaveRequest(String password, String passwordConfirm) {
+		return new UserSaveRequest(LOGIN_ID, EMAIL, password, passwordConfirm);
+	}
+
+	public static UserSaveRequest invalidLoginIdAndEmailUserSaveRequest(String loginId, String email) {
+		return new UserSaveRequest(loginId, email, PASSWORD, PASSWORD);
+	}
+
+	public static UserSaveRequest passwordNotMatchedUserSaveRequest() {
+		return new UserSaveRequest(LOGIN_ID, EMAIL, PASSWORD, NOT_MATCHED_PASSWORD);
 	}
 
 	public static UserSaveServiceRequest passwordNotMatchedUserSaveServiceRequest() {
 		return new UserSaveServiceRequest(LOGIN_ID, EMAIL, PASSWORD, NOT_MATCHED_PASSWORD, null);
 	}
 
-	public static UserLoginServiceRequest userLoginServiceRequest(){
+	public static UserSaveServiceResponse userSaveServiceResponse() {
+		return new UserSaveServiceResponse(1L, LOGIN_ID, EMAIL);
+	}
+
+	public static UserLoginServiceRequest userLoginServiceRequest() {
 		return new UserLoginServiceRequest(LOGIN_ID, PASSWORD);
 	}
+
 }
