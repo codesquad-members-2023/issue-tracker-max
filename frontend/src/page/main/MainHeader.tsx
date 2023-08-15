961865983f1b0e2317b6c0af96956a5e41062073
Jay <chlwodnjs34@gmail.com>
2023-08-15 19:31:18 +0900

    ✨feat #201 : filter 초기화 버튼 구현

--- frontend/src/page/main/MainHeader.tsx
+++ frontend/src/page/main/MainHeader.tsx
@@ -3,7 +3,7 @@ import { styled } from "styled-components";
 import { Button } from "../../components/Button";
 import { FilterBar } from "../../components/FilterBar";
 import { TabButton } from "../../components/TabButton";
-import { IconType } from "../../components/icon/Icon";
+import { Icon, IconType } from "../../components/icon/Icon";
 import { SingleFilterData } from "./Main";
 
 type MainHeaderProps = {
@@ -33,6 +33,7 @@ export function MainHeader({
   handleFilterInput,
 }: MainHeaderProps) {
   const navigate = useNavigate();
+  const queryString = window.location.search;
 
   const tabs: Tab[] = [
     {
@@ -78,45 +79,59 @@ export function MainHeader({
     }
   };
 
+  const navigateToNoneFilter = () => {
+    navigate("/");
+  };
+
   return (
     <Div>
-      <div>
-        <FilterBar
-          name="필터"
-          optionTitle={`이슈 필터`}
-          options={setDropdownOptions(singleFilters)}
-          onChange={onChangeFilterInput}
-          value={input}
-          onBlur={handleFilterInput}
-          onKeyDown={handleEnterKeyPress}
-        />
-      </div>
-      <div style={{ display: "flex", gap: "16px" }}>
-        <TabButton>
-          {tabs.map(({ name, icon, selected }, index) => (
-            <Button
-              key={`tab-${index}`}
-              icon={icon}
-              size="M"
-              buttonType="Ghost"
-              flexible="Flexible"
-              selected={selected}
-              onClick={() => onTabClick(name)}
-            >
-              {name}
-            </Button>
-          ))}
-        </TabButton>
-        <Button
-          icon="Plus"
-          size="S"
-          buttonType="Container"
-          selected
-          onClick={() => navigate("/issues/new")}
-        >
-          이슈 작성
-        </Button>
-      </div>
+      <HeaderWrapper>
+        <div>
+          <FilterBar
+            name="필터"
+            optionTitle={`이슈 필터`}
+            options={setDropdownOptions(singleFilters)}
+            onChange={onChangeFilterInput}
+            value={input}
+            onBlur={handleFilterInput}
+            onKeyDown={handleEnterKeyPress}
+          />
+        </div>
+        <div style={{ display: "flex", gap: "16px" }}>
+          <TabButton>
+            {tabs.map(({ name, icon, selected }, index) => (
+              <Button
+                key={`tab-${index}`}
+                icon={icon}
+                size="M"
+                buttonType="Ghost"
+                flexible="Flexible"
+                selected={selected}
+                onClick={() => onTabClick(name)}
+              >
+                {name}
+              </Button>
+            ))}
+          </TabButton>
+          <Button
+            icon="Plus"
+            size="S"
+            buttonType="Container"
+            selected
+            onClick={() => navigate("/issues/new")}
+          >
+            이슈 작성
+          </Button>
+        </div>
+      </HeaderWrapper>
+      {queryString && (
+        <ClearFilterDiv>
+          <a onClick={navigateToNoneFilter}>
+            <Icon name="XSquare" />
+            <span>현재의 검색 필터 및 정렬 지우기</span>
+          </a>
+        </ClearFilterDiv>
+      )}
     </Div>
   );
 }
@@ -124,7 +139,31 @@ export function MainHeader({
 const Div = styled.div`
   width: 100%;
   display: flex;
+  flex-direction: column;
+  padding-bottom: 24px;
+`;
+
+const HeaderWrapper = styled.div`
+  width: 100%;
+  display: flex;
   justify-content: space-between;
+`;
+
+const ClearFilterDiv = styled.div`
+  width: 100%;
+  height: 32px;
+  display: flex;
   align-items: center;
-  margin-bottom: 24px;
+  gap: 4px;
+  padding-top: 24px;
+
+  & a {
+    display: flex;
+    cursor: pointer;
+  }
+
+  & span {
+    color: ${({ theme }) => theme.color.neutralTextDefault};
+    font: ${({ theme }) => theme.font.displayMedium12};
+  }
 `;
