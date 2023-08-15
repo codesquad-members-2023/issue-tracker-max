1e2bed94e08bca508a65ce9dbd0585873cc5917f
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-08-15 17:53:34 +0900

    🎨style #210: 필요없는 인터셉터 제거

--- backend/src/main/java/codesquard/app/config/WebConfig.java
+++ backend/src/main/java/codesquard/app/config/WebConfig.java
@@ -3,14 +3,11 @@ package codesquard.app.config;
 import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import codesquard.app.authenticate_user.resolver.LoginUserArgumentResolver;
-import codesquard.app.user.interceptor.UserLoginInterceptor;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -28,12 +25,5 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
 		resolvers.add(new LoginUserArgumentResolver());
 		WebMvcConfigurer.super.addArgumentResolvers(resolvers);
-	}
-
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(new UserLoginInterceptor())
-			.order(Ordered.HIGHEST_PRECEDENCE)
-			.addPathPatterns("/api/login");
 	}
 }
