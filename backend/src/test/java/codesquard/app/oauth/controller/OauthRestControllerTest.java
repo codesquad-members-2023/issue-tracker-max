c88bbea2d35547c2ad99eb7f20f5800c47e09ac4
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-08-16 14:05:24 +0900

    ✅test #210: FixedFactory 객체를 이용하여 테스트 코드 수정

--- backend/src/test/java/codesquard/app/oauth/controller/OauthRestControllerTest.java
+++ backend/src/test/java/codesquard/app/oauth/controller/OauthRestControllerTest.java
@@ -1,6 +1,8 @@
 package codesquard.app.oauth.controller;
 
 import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -16,8 +18,7 @@ import codesquard.app.ControllerTestSupport;
 import codesquard.app.api.errors.errorcode.OauthErrorCode;
 import codesquard.app.api.errors.exception.RestApiException;
 import codesquard.app.api.errors.handler.GlobalExceptionHandler;
-import codesquard.app.authenticate_user.entity.AuthenticateUser;
-import codesquard.app.jwt.Jwt;
+import codesquard.app.oauth.fixture.FixedAuthenticateUserFactory;
 import codesquard.app.oauth.service.response.OauthLoginServiceResponse;
 
 class OauthRestControllerTest extends ControllerTestSupport {
@@ -35,16 +36,13 @@ class OauthRestControllerTest extends ControllerTestSupport {
 	@DisplayName("provider 이름과 인가 코드가 주어지고 github 소셜 로그인을 요청할때 로그인된 정보가 응답된다")
 	public void login() throws Exception {
 		// given
-		String provider = "github";
-		String authorizationCode = "c9a29e225ba316e155c2";
-		AuthenticateUser user = new AuthenticateUser(1L, "hong1234", "hong1234@gmail.com", null);
-		Jwt jwt = new Jwt("accessToken", "refreshToken", null, null);
-		OauthLoginServiceResponse loginServiceResponse = new OauthLoginServiceResponse(user, jwt, "Bearer");
+		String authorizationCode = "code";
+		OauthLoginServiceResponse response = FixedAuthenticateUserFactory.oauthLoginServiceResponse();
+
 		// mocking
-		Mockito.when(oauthService.login(Mockito.any(String.class), Mockito.any(String.class)))
-			.thenReturn(loginServiceResponse);
+		when(oauthService.login(any(), any())).thenReturn(response);
 		// when & then
-		mockMvc.perform(post("/api/login/oauth/" + provider)
+		mockMvc.perform(post("/api/login/oauth/github")
 				.param("code", authorizationCode))
 			.andDo(print())
 			.andExpect(jsonPath("code").value(equalTo(200)))
@@ -63,13 +61,12 @@ class OauthRestControllerTest extends ControllerTestSupport {
 	@DisplayName("잘못된 인가 코드가 주어지고 github 소셜 로그인을 요청할때 에러를 응답합니다")
 	public void login_givenInvalidAuthorizationCode_whenRequestOauthLoginApi_thenResponseError400() throws Exception {
 		// given
-		String provider = "github";
-		String authorizationCode = "c9a29e225ba316e155c2";
+		String authorizationCode = "invalidCode";
 		// mocking
-		Mockito.when(oauthService.login(Mockito.any(String.class), Mockito.any(String.class)))
+		when(oauthService.login(Mockito.any(), Mockito.any()))
 			.thenThrow(new RestApiException(OauthErrorCode.BAD_AUTHORIZATION_CODE));
 		// when & then
-		mockMvc.perform(post("/api/login/oauth/" + provider)
+		mockMvc.perform(post("/api/login/oauth/github")
 				.param("code", authorizationCode))
 			.andDo(print())
 			.andExpect(jsonPath("code").value(equalTo(400)))
