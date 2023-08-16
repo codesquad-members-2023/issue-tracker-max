953a4f4c79c1bb4ed485bb8056c7d471f6ca284c
DOEKYONG <apochmn@naver.com>
2023-08-16 18:43:30 +0900

    #96 feat : 이슈 목록 필터링 및 조회

--- be/issue/src/main/java/codesquad/issueTracker/issue/service/IssueService.java
+++ be/issue/src/main/java/codesquad/issueTracker/issue/service/IssueService.java
@@ -1,7 +1,5 @@
 package codesquad.issueTracker.issue.service;
 
-import codesquad.issueTracker.issue.vo.IssueMileStoneDetailVo;
-import codesquad.issueTracker.issue.vo.IssueUserVo;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashSet;
@@ -23,7 +21,11 @@ import codesquad.issueTracker.global.common.Status;
 import codesquad.issueTracker.global.exception.CustomException;
 import codesquad.issueTracker.global.exception.ErrorCode;
 import codesquad.issueTracker.issue.domain.Issue;
+import codesquad.issueTracker.issue.domain.IssueRead;
+import codesquad.issueTracker.issue.domain.IssueSearch;
+import codesquad.issueTracker.issue.dto.IssueCountResponseDto;
 import codesquad.issueTracker.issue.dto.IssueFileResponseDto;
+import codesquad.issueTracker.issue.dto.IssueFilteredResponseDto;
 import codesquad.issueTracker.issue.dto.IssueLabelResponseDto;
 import codesquad.issueTracker.issue.dto.IssueMilestoneResponseDto;
 import codesquad.issueTracker.issue.dto.IssueOptionResponseDto;
@@ -38,10 +40,13 @@ import codesquad.issueTracker.issue.dto.ModifyIssueStatusRequestDto;
 import codesquad.issueTracker.issue.dto.ModifyIssueTitleRequest;
 import codesquad.issueTracker.issue.dto.ModifyIssueTitleResponse;
 import codesquad.issueTracker.issue.dto.ModifyLabelRequestDto;
+import codesquad.issueTracker.issue.repository.IssueMapperRepository;
 import codesquad.issueTracker.issue.repository.IssueRepository;
 import codesquad.issueTracker.issue.vo.AssigneeVo;
 import codesquad.issueTracker.issue.vo.IssueLabelVo;
+import codesquad.issueTracker.issue.vo.IssueMileStoneDetailVo;
 import codesquad.issueTracker.issue.vo.IssueMilestoneVo;
+import codesquad.issueTracker.issue.vo.IssueUserVo;
 import codesquad.issueTracker.label.dto.LabelResponseDto;
 import codesquad.issueTracker.label.service.LabelService;
 import codesquad.issueTracker.milestone.service.MilestoneService;
@@ -55,229 +60,239 @@ import lombok.RequiredArgsConstructor;
 @Transactional(readOnly = true)
 public class IssueService {
 
-    @Value("${cloud.aws.s3.bucket}")
-    private String bucketName;
-    private final IssueRepository issueRepository;
-    private final LabelService labelService;
-    private final UserService userService;
-    private final MilestoneService milestoneService;
-    private final AmazonS3Client amazonS3Client;
+	@Value("${cloud.aws.s3.bucket}")
+	private String bucketName;
+	private final IssueRepository issueRepository;
+	private final LabelService labelService;
+	private final UserService userService;
+	private final MilestoneService milestoneService;
+	private final AmazonS3Client amazonS3Client;
+	private final IssueMapperRepository issueMapperRepository;
 
-    @Transactional
-    public Long save(IssueWriteRequestDto request, Long id) {
-        milestoneService.isExistMilestone(request.getMilestoneId());
-        List<Long> labels = request.getLabels();
-        List<Long> assignees = request.getAssignees();
-        Issue issue = IssueWriteRequestDto.toEntity(request, id);
-        Long savedIssueId = issueRepository.insert(issue);
+	public IssueFilteredResponseDto findByFilter(IssueSearch issueSearch) {
+		// 1. count 가져오는 로직도 하나 필요
+		IssueCountResponseDto issueCountResponseDto = issueRepository.findIssueCounts();
+		// 2. issueReads 가져오기
+		List<IssueRead> issueReads = issueMapperRepository.findFilteredIssue(issueSearch);
+		// response에서 1,2를 묶어주는 메서드 필요
+		return IssueFilteredResponseDto.of(issueCountResponseDto, issueReads);
+	}
 
-        // 라벨 리스트가 null 이 아니면 해당 라벨이 존재하는지 검증 후  라벨과 이슈 연결 테이블에 insert
-        if (labels != null) {
-            duplicatedId(labels);
-            labels.stream()
-                    .map(labelId -> labelService.validateLabelsId(labelId))
-                    .map(existLabel -> issueRepository.insertLabels(savedIssueId, existLabel.getId()))
-                    .collect(Collectors.toList());
-        }
+	@Transactional
+	public Long save(IssueWriteRequestDto request, Long id) {
+		milestoneService.isExistMilestone(request.getMilestoneId());
+		List<Long> labels = request.getLabels();
+		List<Long> assignees = request.getAssignees();
+		Issue issue = IssueWriteRequestDto.toEntity(request, id);
+		Long savedIssueId = issueRepository.insert(issue);
 
-        // assignee 리스트가 null 이 아니면 assignees( 유저 id )가  존재하는지 검증 후  assignees 테이블에 insert
-        if (assignees != null) {
-            duplicatedId(assignees);
-            assignees.stream()
-                    .map(assigneesId -> userService.validateUserId(assigneesId))
-                    .map(existUser -> issueRepository.insertAssignees(savedIssueId, existUser.getId()))
-                    .collect(Collectors.toList());
-        }
-        return savedIssueId;
-    }
+		// 라벨 리스트가 null 이 아니면 해당 라벨이 존재하는지 검증 후  라벨과 이슈 연결 테이블에 insert
+		if (labels != null) {
+			duplicatedId(labels);
+			labels.stream()
+				.map(labelId -> labelService.validateLabelsId(labelId))
+				.map(existLabel -> issueRepository.insertLabels(savedIssueId, existLabel.getId()))
+				.collect(Collectors.toList());
+		}
 
-    private void duplicatedId(List<Long> list) {
-        Set<Long> set = new HashSet<>();
-        for (Long temp : list) {
-            if (!set.add(temp)) {
-                throw new CustomException(ErrorCode.DUPLICATE_OBJECT_FOUND);
-            }
-        }
-    }
+		// assignee 리스트가 null 이 아니면 assignees( 유저 id )가  존재하는지 검증 후  assignees 테이블에 insert
+		if (assignees != null) {
+			duplicatedId(assignees);
+			assignees.stream()
+				.map(assigneesId -> userService.validateUserId(assigneesId))
+				.map(existUser -> issueRepository.insertAssignees(savedIssueId, existUser.getId()))
+				.collect(Collectors.toList());
+		}
+		return savedIssueId;
+	}
 
-    public IssueLabelResponseDto getIssueLabels() {
-        LabelResponseDto allLabels = labelService.findAll();
-        List<IssueLabelVo> labels = allLabels.getLabels().stream()
-                .map(IssueLabelVo::from)
-                .collect(Collectors.toList());
-        return IssueLabelResponseDto.from(labels);
-    }
+	private void duplicatedId(List<Long> list) {
+		Set<Long> set = new HashSet<>();
+		for (Long temp : list) {
+			if (!set.add(temp)) {
+				throw new CustomException(ErrorCode.DUPLICATE_OBJECT_FOUND);
+			}
+		}
+	}
 
-    public IssueMilestoneResponseDto getIssueMilestones() {
-        List<MilestoneVo> milestones = milestoneService.findMilestonesByStatus(Status.OPEN.getStatus());
-        List<IssueMileStoneDetailVo> milestoneVos = milestones.stream()
-                .map(IssueMileStoneDetailVo::from)
-                .collect(Collectors.toList());
-        return IssueMilestoneResponseDto.from(milestoneVos);
-    }
+	public IssueLabelResponseDto getIssueLabels() {
+		LabelResponseDto allLabels = labelService.findAll();
+		List<IssueLabelVo> labels = allLabels.getLabels().stream()
+			.map(IssueLabelVo::from)
+			.collect(Collectors.toList());
+		return IssueLabelResponseDto.from(labels);
+	}
 
-    public IssueUserResponseDto getIssueUsers() {
-        List<User> users = userService.getUsers();
-        List<IssueUserVo> participants = users.stream()
-                .map(IssueUserVo::from)
-                .collect(Collectors.toList());
-        return IssueUserResponseDto.from(participants);
-    }
+	public IssueMilestoneResponseDto getIssueMilestones() {
+		List<MilestoneVo> milestones = milestoneService.findMilestonesByStatus(Status.OPEN.getStatus());
+		List<IssueMileStoneDetailVo> milestoneVos = milestones.stream()
+			.map(IssueMileStoneDetailVo::from)
+			.collect(Collectors.toList());
+		return IssueMilestoneResponseDto.from(milestoneVos);
+	}
 
-    public IssueResponseDto getIssueById(Long issueId) {
-        validateExistIssue(issueId);
-        Issue issue = validateActiveIssueById(issueId);
+	public IssueUserResponseDto getIssueUsers() {
+		List<User> users = userService.getUsers();
+		List<IssueUserVo> participants = users.stream()
+			.map(IssueUserVo::from)
+			.collect(Collectors.toList());
+		return IssueUserResponseDto.from(participants);
+	}
 
-        return IssueResponseDto.from(issue);
-    }
+	public IssueResponseDto getIssueById(Long issueId) {
+		validateExistIssue(issueId);
+		Issue issue = validateActiveIssueById(issueId);
 
-    private Issue validateActiveIssueById(Long issueId) {
-        return issueRepository.findActiveIssueById(issueId)
-                .orElseThrow(() -> new CustomException(ErrorCode.ALREADY_DELETED_ISSUE));
-    }
+		return IssueResponseDto.from(issue);
+	}
 
-    public IssueOptionResponseDto getIssueOptions(Long issueId) {
-        validateExistIssue(issueId);
-        validateActiveIssueById(issueId);
+	private Issue validateActiveIssueById(Long issueId) {
+		return issueRepository.findActiveIssueById(issueId)
+			.orElseThrow(() -> new CustomException(ErrorCode.ALREADY_DELETED_ISSUE));
+	}
 
-        List<AssigneeVo> assignees = issueRepository.findAssigneesById(issueId);
-        List<IssueLabelVo> labels = labelService.findByIssueId(issueId);
-        IssueMilestoneVo milestone = milestoneService.findByIssueId(issueId);
+	public IssueOptionResponseDto getIssueOptions(Long issueId) {
+		validateExistIssue(issueId);
+		validateActiveIssueById(issueId);
 
-        if (milestone.getIssueMileStoneDetailVo().getId() != null) {
-            int closeCount = issueRepository.findCountByStatusAndMilestone(Status.CLOSED.getStatus(), milestone);
-            int openCount = issueRepository.findCountByStatusAndMilestone(Status.OPEN.getStatus(), milestone);
-            return IssueOptionResponseDto.of(assignees, labels, milestone.getMilestoneWithRatio(openCount, closeCount));
-        }
+		List<AssigneeVo> assignees = issueRepository.findAssigneesById(issueId);
+		List<IssueLabelVo> labels = labelService.findByIssueId(issueId);
+		IssueMilestoneVo milestone = milestoneService.findByIssueId(issueId);
 
-        return IssueOptionResponseDto.of(assignees, labels, milestone);
-    }
+		if (milestone.getIssueMileStoneDetailVo().getId() != null) {
+			int closeCount = issueRepository.findCountByStatusAndMilestone(Status.CLOSED.getStatus(), milestone);
+			int openCount = issueRepository.findCountByStatusAndMilestone(Status.OPEN.getStatus(), milestone);
+			return IssueOptionResponseDto.of(assignees, labels, milestone.getMilestoneWithRatio(openCount, closeCount));
+		}
 
-    @Transactional
-    public List<Long> modifyIssueStatus(ModifyIssueStatusRequestDto request) {
-        List<Long> issueIds = request.getIssueIds();
-        Boolean status = Status.from(request.getStatus()).getStatus();
-        if (issueIds != null) {
-            duplicatedId(issueIds);
-            issueIds.stream()
-                    .map(issueId -> validateExistActiveIssue(issueId))
-                    .map(existIssue -> issueRepository.modifyStatus(existIssue.getId(), status))
-                    .collect(Collectors.toList());
-        }
-        return issueIds;
-    }
+		return IssueOptionResponseDto.of(assignees, labels, milestone);
+	}
 
-    @Transactional
-    public Long modifyIssueStatusInDetail(Long id, ModifyIssueStatusRequestDto request) {
-        Boolean status = Status.from(request.getStatus()).getStatus();
-        validateExistActiveIssue(id);
-        return issueRepository.modifyStatus(id, status);
-    }
+	@Transactional
+	public List<Long> modifyIssueStatus(ModifyIssueStatusRequestDto request) {
+		List<Long> issueIds = request.getIssueIds();
+		Boolean status = Status.from(request.getStatus()).getStatus();
+		if (issueIds != null) {
+			duplicatedId(issueIds);
+			issueIds.stream()
+				.map(issueId -> validateExistActiveIssue(issueId))
+				.map(existIssue -> issueRepository.modifyStatus(existIssue.getId(), status))
+				.collect(Collectors.toList());
+		}
+		return issueIds;
+	}
 
-    private Issue validateExistActiveIssue(Long issuesIds) {
-        return issueRepository.findActiveIssueById(issuesIds)
-                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_ISSUES));
-    }
+	@Transactional
+	public Long modifyIssueStatusInDetail(Long id, ModifyIssueStatusRequestDto request) {
+		Boolean status = Status.from(request.getStatus()).getStatus();
+		validateExistActiveIssue(id);
+		return issueRepository.modifyStatus(id, status);
+	}
 
-    private void validateExistIssue(Long issueId) {
-        issueRepository.findById(issueId)
-                .orElseThrow(() -> new CustomException(ErrorCode.NOT_EXIST_ISSUE));
-    }
+	private Issue validateExistActiveIssue(Long issuesIds) {
+		return issueRepository.findActiveIssueById(issuesIds)
+			.orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_ISSUES));
+	}
 
-    @Transactional
-    public ModifyIssueContentResponseDto modifyIssueContent(Long id, ModifyIssueContentRequestDto request) {
-        validateExistActiveIssue(id);
-        String modifiedContent = request.getContent();
-        issueRepository.updateContent(id, modifiedContent);
-        return new ModifyIssueContentResponseDto(modifiedContent);
-    }
+	private void validateExistIssue(Long issueId) {
+		issueRepository.findById(issueId)
+			.orElseThrow(() -> new CustomException(ErrorCode.NOT_EXIST_ISSUE));
+	}
 
-    @Transactional
-    public ModifyIssueTitleResponse modifyIssueTitle(Long id, ModifyIssueTitleRequest request) {
-        validateExistActiveIssue(id);
-        String modifiedTitle = request.getTitle();
-        issueRepository.updateTitle(id, modifiedTitle);
-        return new ModifyIssueTitleResponse(modifiedTitle);
-    }
+	@Transactional
+	public ModifyIssueContentResponseDto modifyIssueContent(Long id, ModifyIssueContentRequestDto request) {
+		validateExistActiveIssue(id);
+		String modifiedContent = request.getContent();
+		issueRepository.updateContent(id, modifiedContent);
+		return new ModifyIssueContentResponseDto(modifiedContent);
+	}
 
-    @Transactional
-    public Long delete(Long id) {
-        validateExistActiveIssue(id);
-        Long deletedId = issueRepository.delete(id);
-        return deletedId;
+	@Transactional
+	public ModifyIssueTitleResponse modifyIssueTitle(Long id, ModifyIssueTitleRequest request) {
+		validateExistActiveIssue(id);
+		String modifiedTitle = request.getTitle();
+		issueRepository.updateTitle(id, modifiedTitle);
+		return new ModifyIssueTitleResponse(modifiedTitle);
+	}
 
-    }
+	@Transactional
+	public Long delete(Long id) {
+		validateExistActiveIssue(id);
+		Long deletedId = issueRepository.delete(id);
+		return deletedId;
 
-    @Transactional
-    public Long modifyAssignees(Long id, ModifyAssigneeRequestDto request) {
-        validateExistActiveIssue(id);
-        List<Long> assignees = request.getAssignees();
+	}
 
-        if (assignees != null) {
-            duplicatedId(assignees);
-            for (Long assigneeId : assignees) {
-                userService.validateUserId(assigneeId);
-            }
-            issueRepository.resetAssignees(id);
-            for (Long assigneeId : assignees) {
-                issueRepository.insertAssignees(id, assigneeId);
-            }
-            return id;
-        }
-        issueRepository.resetAssignees(id);
-        return id;
-    }
+	@Transactional
+	public Long modifyAssignees(Long id, ModifyAssigneeRequestDto request) {
+		validateExistActiveIssue(id);
+		List<Long> assignees = request.getAssignees();
 
-    @Transactional
-    public Long modifyLabels(Long id, ModifyLabelRequestDto request) {
-        validateExistActiveIssue(id);
-        List<Long> labels = request.getLabels();
+		if (assignees != null) {
+			duplicatedId(assignees);
+			for (Long assigneeId : assignees) {
+				userService.validateUserId(assigneeId);
+			}
+			issueRepository.resetAssignees(id);
+			for (Long assigneeId : assignees) {
+				issueRepository.insertAssignees(id, assigneeId);
+			}
+			return id;
+		}
+		issueRepository.resetAssignees(id);
+		return id;
+	}
 
-        if (labels != null) {
-            duplicatedId(labels);
-            for (Long labelId : labels) {
-                labelService.validateLabelsId(labelId);
-            }
-            labelService.resetLabels(id);
-            for (Long labelId : labels) {
-                labelService.insertIssuesLabels(id, labelId);
-            }
-            return id;
-        }
-        labelService.resetLabels(id);
-        return id;
-    }
+	@Transactional
+	public Long modifyLabels(Long id, ModifyLabelRequestDto request) {
+		validateExistActiveIssue(id);
+		List<Long> labels = request.getLabels();
 
-    @Transactional
-    public Long modifyMilestone(Long id, ModifyIssueMilestoneDto request) {
-        validateExistActiveIssue(id);
-        Long milestoneId = request.getMilestone();
-        if (milestoneId != null) {
-            milestoneService.isExistMilestone(milestoneId);
-        }
-        return issueRepository.updateMilestone(id, milestoneId);
-    }
+		if (labels != null) {
+			duplicatedId(labels);
+			for (Long labelId : labels) {
+				labelService.validateLabelsId(labelId);
+			}
+			labelService.resetLabels(id);
+			for (Long labelId : labels) {
+				labelService.insertIssuesLabels(id, labelId);
+			}
+			return id;
+		}
+		labelService.resetLabels(id);
+		return id;
+	}
 
-    public IssueFileResponseDto uploadImg(MultipartFile multipartFile) {
-        validateFileExists(multipartFile);
-        String fileName = multipartFile.getOriginalFilename();
-        ObjectMetadata objectMetadata = new ObjectMetadata();
-        objectMetadata.setContentType(multipartFile.getContentType());
+	@Transactional
+	public Long modifyMilestone(Long id, ModifyIssueMilestoneDto request) {
+		validateExistActiveIssue(id);
+		Long milestoneId = request.getMilestone();
+		if (milestoneId != null) {
+			milestoneService.isExistMilestone(milestoneId);
+		}
+		return issueRepository.updateMilestone(id, milestoneId);
+	}
 
-        try (InputStream inputStream = multipartFile.getInputStream()) {
-            amazonS3Client.putObject(new PutObjectRequest(bucketName, fileName, inputStream, objectMetadata)
-                    .withCannedAcl(CannedAccessControlList.PublicRead));
-        } catch (IOException e) {
-            throw new CustomException(ErrorCode.FAILED_UPLOAD_FILE);
-        }
-        String url = amazonS3Client.getUrl(bucketName, fileName).toString();
-        IssueFileResponseDto response = new IssueFileResponseDto(url);
-        return response;
-    }
+	public IssueFileResponseDto uploadImg(MultipartFile multipartFile) {
+		validateFileExists(multipartFile);
+		String fileName = multipartFile.getOriginalFilename();
+		ObjectMetadata objectMetadata = new ObjectMetadata();
+		objectMetadata.setContentType(multipartFile.getContentType());
 
-    private void validateFileExists(MultipartFile multipartFile) {
-        if (multipartFile.isEmpty()) {
-            throw new CustomException(ErrorCode.EMPTY_FILE_EXCEPTION);
-        }
-    }
+		try (InputStream inputStream = multipartFile.getInputStream()) {
+			amazonS3Client.putObject(new PutObjectRequest(bucketName, fileName, inputStream, objectMetadata)
+				.withCannedAcl(CannedAccessControlList.PublicRead));
+		} catch (IOException e) {
+			throw new CustomException(ErrorCode.FAILED_UPLOAD_FILE);
+		}
+		String url = amazonS3Client.getUrl(bucketName, fileName).toString();
+		IssueFileResponseDto response = new IssueFileResponseDto(url);
+		return response;
+	}
+
+	private void validateFileExists(MultipartFile multipartFile) {
+		if (multipartFile.isEmpty()) {
+			throw new CustomException(ErrorCode.EMPTY_FILE_EXCEPTION);
+		}
+	}
 }
