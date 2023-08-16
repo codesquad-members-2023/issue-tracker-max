0235ba0235f9f1a4e289aaeb4d0a00365cf9fe4c
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-08-16 19:43:58 +0900

    🐛fix #217: cors 패턴 수정

--- backend/src/main/java/codesquard/app/config/WebConfig.java
+++ backend/src/main/java/codesquard/app/config/WebConfig.java
@@ -18,7 +18,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOriginPatterns("http://localhost:5173/*")
+			.allowedOriginPatterns("http://localhost:5173", "http://localhost:8080", "http://localhost:80")
 			.allowedHeaders("*")
 			.allowCredentials(true) // 쿠키 인증 요청 허용
 			.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
