8d4d421c2b1b9982587b1791c4507145fdd4a6e7
jsh3418 <albert5428@gmail.com>
2023-08-17 17:42:35 +0900

    [FE] feat: 로그아웃 기능 구현

--- fe/src/components/header/Header.tsx
+++ fe/src/components/header/Header.tsx
@@ -1,49 +1,44 @@
 import { Outlet, useNavigate } from 'react-router-dom';
 import { ReactComponent as MediumLogo } from '@assets/logos/mediumLogo.svg';
-import { useTheme } from '@emotion/react';
+import { Theme, css, useTheme } from '@emotion/react';
 import { Button } from '@components/common/Button';
 import { ThemeToggle } from './ThemeToggle';
 import { PATH } from 'constants/PATH';
+import { getLocalStorageImage } from 'apis/localStorage';
 
 type Props = {
-  image?: string;
   currentTheme: ThemeType;
   toggleTheme: () => void;
   resetUserId?: () => void;
   resetAccessToken?: () => void;
 };
 
-export const Header: React.FC<Props> = ({
-  image,
-  currentTheme,
-  toggleTheme,
-}) => {
+export const Header: React.FC<Props> = ({ currentTheme, toggleTheme }) => {
   const navigate = useNavigate();
   const theme = useTheme() as any;
 
+  const onLogoutClick = () => {
+    localStorage.clear();
+    navigate(PATH.SIGN_PAGE);
+  };
+
+  const image = getLocalStorageImage();
+
   return (
     <>
-      <header
-        css={{
-          height: '94px',
-          marginBottom: '32px',
-          display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-        }}
-      >
+      <header css={headerStyle}>
         <MediumLogo
+          className="logo"
           fill={theme.neutral.text.strong}
           onClick={() => {
             navigate(PATH.ISSUE_LIST_PAGE);
           }}
-          css={{ cursor: 'pointer' }}
         />
 
-        <div css={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+        <div className="header-right">
           <ThemeToggle {...{ currentTheme, toggleTheme }} />
 
-          <Button typeVariant="ghost" size="S">
+          <Button typeVariant="ghost" size="S" onClick={onLogoutClick}>
             로그아웃
           </Button>
 
@@ -59,3 +54,21 @@ export const Header: React.FC<Props> = ({
     </>
   );
 };
+
+const headerStyle = (theme: Theme) => css`
+  height: 94px;
+  margin-bottom: 32px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .logo {
+    cursor: pointer;
+  }
+
+  .header-right {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+  }
+`;
