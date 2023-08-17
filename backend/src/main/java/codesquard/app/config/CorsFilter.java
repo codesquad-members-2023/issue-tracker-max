9058909727f26fbd8071d5d369d179cd26940ba7
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-08-17 21:55:50 +0900

    🐛fix #253: 추가적인 origin 설정

--- backend/src/main/java/codesquard/app/config/CorsFilter.java
+++ backend/src/main/java/codesquard/app/config/CorsFilter.java
@@ -24,7 +24,7 @@ public class CorsFilter implements Filter {
 		HttpServletRequest request = (HttpServletRequest)req;
 		HttpServletResponse response = (HttpServletResponse)res;
 
-		response.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+		response.setHeader("Access-Control-Allow-Origin", "http://localhost:5173, http://3.36.120.124:5173");
 		response.setHeader("Access-Control-Allow-Credentials", "true");
 		response.setHeader("Access-Control-Allow-Methods", "*");
 		response.setHeader("Access-Control-Max-Age", "3600");
