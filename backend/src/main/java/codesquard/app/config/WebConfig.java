ec63788f043c0350ac70c9092a69e51b7b77eb29
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-08-17 11:30:44 +0900

    🐛fix #217: cors 롤백

--- backend/src/main/java/codesquard/app/config/WebConfig.java
+++ backend/src/main/java/codesquard/app/config/WebConfig.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,15 +14,15 @@ import codesquard.app.user.interceptor.UserLoginInterceptor;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/api/**")
-			.allowedOriginPatterns("http://3.36.120.124/*", "http://3.36.120.124:5173/*", "http://localhost:5173/*")
-			.exposedHeaders("Authorization") // 노출될 헤더 설정
-			.allowCredentials(true) // 쿠키 인증 요청 허용
-			.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
-			.maxAge(3600);
-	}
+	// @Override
+	// public void addCorsMappings(CorsRegistry registry) {
+	// 	registry.addMapping("/api/**")
+	// 		.allowedOriginPatterns("http://3.36.120.124", "http://3.36.120.124:5173", "http://localhost:5173")
+	// 		.exposedHeaders("Authorization") // 노출될 헤더 설정
+	// 		.allowCredentials(true) // 쿠키 인증 요청 허용
+	// 		.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+	// 		.maxAge(3600);
+	// }
 
 	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
