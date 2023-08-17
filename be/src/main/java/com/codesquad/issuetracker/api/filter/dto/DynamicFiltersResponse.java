bbb418d5f40142d1de94a40183c362a1fdb1c0d9
CDBchan <kdjin1215@naver.com>
2023-08-17 18:22:32 +0900

    refactor: 동적필터 필요없는 응답 삭제

--- be/src/main/java/com/codesquad/issuetracker/api/filter/dto/DynamicFiltersResponse.java
+++ be/src/main/java/com/codesquad/issuetracker/api/filter/dto/DynamicFiltersResponse.java
@@ -21,10 +21,5 @@ public class DynamicFiltersResponse {
         this.labels = labels;
         this.milestones = milestones;
         this.authors = authors;
-
-        // TODO: 현재는 임시로 처리한 방법
-        assignees.add(new IssueAssigneeVo(0L, "담당자가 없는 이슈", null));
-        labels.add(new LabelFilter(0L, "레이블이 없는 이슈", null, null));
-        milestones.add(new MilestoneResponse(0L, "마일스톤이 없는 이슈"));
     }
 }
