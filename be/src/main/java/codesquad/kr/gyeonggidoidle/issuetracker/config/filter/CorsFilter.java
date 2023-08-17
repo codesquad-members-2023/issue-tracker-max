57b3558eecee2968cbdc7658c0ef4163e7043f79
HeeJu Cho <121915790+he2joojo@users.noreply.github.com>
2023-08-17 17:15:12 +0900

    fix: cors문제로 헤더 추가

--- be/src/main/java/codesquad/kr/gyeonggidoidle/issuetracker/config/filter/CorsFilter.java
+++ be/src/main/java/codesquad/kr/gyeonggidoidle/issuetracker/config/filter/CorsFilter.java
@@ -16,9 +16,10 @@ public class CorsFilter implements Filter {
 
         HttpServletResponse response = (HttpServletResponse) res;
 
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
         response.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS, GET, DELETE, PUT, PATCH");
         response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Headers",
                 "Authorization, x-requested-with, origin, content-type, accept");
         chain.doFilter(req, res);
