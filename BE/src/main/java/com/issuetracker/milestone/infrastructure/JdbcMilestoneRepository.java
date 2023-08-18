f1733efec7929cccd5686313500a3b9280b63e7e
Mandu <rjswmtk@gmail.com>
2023-08-18 12:29:40 +0900

    fix/#201: 이슈 필터 및 이슈 상세 조회 시 마일스톤 진척도 추가 (#206)

--- BE/src/main/java/com/issuetracker/milestone/infrastructure/JdbcMilestoneRepository.java
+++ BE/src/main/java/com/issuetracker/milestone/infrastructure/JdbcMilestoneRepository.java
@@ -22,7 +22,7 @@ public class JdbcMilestoneRepository implements MilestoneRepository {
 
 	private static final String EXIST_BY_ID_SQL = "SELECT EXISTS(SELECT 1 FROM milestone WHERE is_deleted = false AND id = :id)";
 	private static final String EXIST_BY_TITLE_SQL = "SELECT EXISTS(SELECT 1 FROM milestone WHERE is_deleted = false AND title = :title)";
-	private static final String FIND_ALL_FOR_FILTER = "SELECT id, title FROM milestone WHERE milestone.is_deleted = false  ORDER BY title";
+	private static final String FIND_ALL_FOR_FILTER = "SELECT milestone.id, milestone.title, IFNULL((SUM(issue.is_open = false) / COUNT(issue.id) * 100), 0) AS progress FROM milestone LEFT OUTER JOIN issue ON issue.milestone_id = milestone.id WHERE milestone.is_deleted = false GROUP BY milestone.id ORDER BY milestone.title";
 	private static final String SAVE_SQL = "INSERT INTO milestone(title, description, deadline, is_open) VALUE(:title, :description, :deadline, true)";
 	private static final String UPDATE_SQL = "UPDATE milestone SET title = :title, description = :description, deadline = :deadline WHERE id = :id";
 	private static final String UPDATE_OPEN_STATUS_SQL = "UPDATE milestone SET is_open = :isOpen WHERE id = :id";
@@ -105,6 +105,7 @@ public class JdbcMilestoneRepository implements MilestoneRepository {
 				Milestone.builder()
 					.id(rs.getLong("id"))
 					.title(rs.getString("title"))
+					.progress(rs.getInt("progress"))
 					.build();
 
 		return jdbcTemplate.query(FIND_ALL_FOR_FILTER, milestoneRowMapper);
