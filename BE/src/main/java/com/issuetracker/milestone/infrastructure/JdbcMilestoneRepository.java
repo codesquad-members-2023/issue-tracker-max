48f080a0c057db088aabb16e8268685bc4988192
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2023-08-18 11:45:53 +0900

    feat/#176: 이슈 수정용 마일스톤 목록 불러오기 버그 fix - 3

--- BE/src/main/java/com/issuetracker/milestone/infrastructure/JdbcMilestoneRepository.java
+++ BE/src/main/java/com/issuetracker/milestone/infrastructure/JdbcMilestoneRepository.java
@@ -71,7 +71,7 @@ public class JdbcMilestoneRepository implements MilestoneRepository {
 		+ "LEFT JOIN issue "
 		+ "ON milestone.id = issue.milestone_id "
 		+ "WHERE milestone.is_deleted = false "
-		+ "AND milestone.id != COALESCE((SELECT sub_issue.milestone_id FROM issue sub_issue WHERE sub_issue.id = 2), 0) "
+		+ "AND milestone.id != COALESCE((SELECT sub_issue.milestone_id FROM issue sub_issue WHERE sub_issue.id = :issueId), 0) "
 		+ "GROUP BY milestone.id "
 		+ "ORDER BY milestone.title";
 	private static final String CALCULATE_COUNT_METADATA
