7594e5bc9a598378c137eebc7f4ed71f95c182fa
Daeram Chung <daeram.chung@gmail.com>
2023-08-18 11:29:42 +0900

    fix: 환경변수 이름 수정

--- fe/src/components/Auth/GitHubLoginButton.tsx
+++ fe/src/components/Auth/GitHubLoginButton.tsx
@@ -2,7 +2,7 @@ import Button from "@components/common/Button";
 
 export default function GitHubLoginButton() {
   const gitHubCredentialsLink = `https://github.com/login/oauth/authorize?client_id=${
-    import.meta.env.GITHUB_CLIENT_ID
+    import.meta.env.VITE_GITHUB_CLIENT_ID
   }&allow_signup=false&scope=read:user,user:email`;
 
   return (
