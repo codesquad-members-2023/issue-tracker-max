097aaa53077f284203f19162e8900830ecf1f583
jsjseop <57559288+jsjseop@users.noreply.github.com>
2023-08-18 11:47:48 +0900

    [BE] fix: 에러 수정

--- be/issue-tracker/src/main/java/org/presents/issuetracker/global/error/statuscode/JwtErrorCode.java
+++ be/issue-tracker/src/main/java/org/presents/issuetracker/global/error/statuscode/JwtErrorCode.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 public enum JwtErrorCode implements StatusCode {
 	NOT_VALID_LOGIN_INFO(HttpStatus.UNAUTHORIZED, "로그인 정보가 유효하지 않습니다."),
 	INVALID_JWT_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
-	EXPIRED_JWT_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
+	EXPIRED_JWT_TOKEN(HttpStatus.FORBIDDEN, "만료된 토큰입니다."),
 	MALFORMED_JWT_TOKEN(HttpStatus.UNAUTHORIZED, "손상된 토큰입니다.");
 
 	private final HttpStatus httpStatus;
