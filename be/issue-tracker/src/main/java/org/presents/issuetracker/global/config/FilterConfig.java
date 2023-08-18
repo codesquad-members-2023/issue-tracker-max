491badeb2592b4d3fbc2ef05784e57d8531b4a69
jsjseop <57559288+jsjseop@users.noreply.github.com>
2023-08-18 17:13:53 +0900

    [BE] fix: response cors 필터 적용

--- be/issue-tracker/src/main/java/org/presents/issuetracker/global/config/FilterConfig.java
+++ be/issue-tracker/src/main/java/org/presents/issuetracker/global/config/FilterConfig.java
@@ -1,5 +1,6 @@
 package org.presents.issuetracker.global.config;
 
+import org.presents.issuetracker.global.filter.CorsFilter;
 import org.presents.issuetracker.global.filter.JwtAuthorizationFilter;
 import org.presents.issuetracker.jwt.JwtProvider;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
@@ -12,11 +13,19 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class FilterConfig {
 
 	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter() {
+		FilterRegistrationBean<CorsFilter> registrationBean = new FilterRegistrationBean<>();
+		registrationBean.setFilter(new CorsFilter());
+		registrationBean.setOrder(1);
+		return registrationBean;
+	}
+
+	@Bean
 	public FilterRegistrationBean<JwtAuthorizationFilter> jwtAuthorizationFilter(JwtProvider jwtProvider,
 		ObjectMapper objectMapper) {
 		FilterRegistrationBean<JwtAuthorizationFilter> registrationBean = new FilterRegistrationBean<>();
 		registrationBean.setFilter(new JwtAuthorizationFilter(jwtProvider, objectMapper));
-		registrationBean.setOrder(1);
+		registrationBean.setOrder(2);
 		return registrationBean;
 	}
 }
